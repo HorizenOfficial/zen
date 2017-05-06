c3f7dcfbb60b024caae11eac1174b595a2d83f93
joshuayabut <yabut.joshua@gmail.com>
2017-05-06 04:23:24 -0400

    fixup! WIP: #12 - Replay attack prevention

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -12,9 +12,7 @@
 
 #include <boost/foreach.hpp>
 
-#if defined(BITCOIN_ZCASHCONSENSUS_H)
 #include "main.h"
-#endif
 
 using namespace std;
 
@@ -309,7 +307,8 @@ public:
         return false;
     }
 
-#ifndef BITCOIN_ZCASHCONSENSUS_H // zen-tx does not have access to chain state so no replay protection is possible
+
+#if defined(BITCOIN_TX) // zen-tx does not have access to chain state so no replay protection is possible
     bool operator()(const CKeyID &keyID) const {
         script->clear();
         *script << OP_DUP << OP_HASH160 << ToByteVector(keyID) << OP_EQUALVERIFY << OP_CHECKSIG;
@@ -321,6 +320,8 @@ public:
         *script << OP_HASH160 << ToByteVector(scriptID) << OP_EQUAL;
     }
 #else
+
+
     bool operator()(const CKeyID &keyID) const {
         script->clear();
         CBlockIndex *currentBlock = chainActive.Tip();
