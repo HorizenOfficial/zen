22f28d16ca9b4a5b814b59581b6cbbd2790b55ad
joshuayabut <yabut.joshua@gmail.com>
2017-05-23 09:03:29 -0400

    Remove Zcash references in rpcserver.cpp

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -245,10 +245,10 @@ UniValue stop(const UniValue& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Zcash server.");
+            "\nStop Zen server.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    return "Zcash server stopping";
+    return "Zen server stopping";
 }
 
 /**
@@ -581,7 +581,7 @@ UniValue CRPCTable::execute(const std::string &strMethod, const UniValue &params
 
 std::string HelpExampleCli(const std::string& methodname, const std::string& args)
 {
-    return "> zcash-cli " + methodname + " " + args + "\n";
+    return "> zen-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
