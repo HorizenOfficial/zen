0eb1b256f6cb44dc82382c0f83dfc5927eb8eeed
Dmytro Kaidalov <dmtr.kd@gmail.com>
2017-09-14 12:21:13 +0300

    TLS: fix runtime exception in LogPrintf

--- src/utiltls.cpp
+++ src/utiltls.cpp
@@ -404,7 +404,7 @@ bool ValidatePeerCertificate(SSL *ssl)
     }
     else
     {
-        LogPrintf ("net", "TLS: Peer does not have certificate\n");
+        LogPrint("net", "TLS: Peer does not have certificate\n");
         bIsOk = false;
     }
     return bIsOk;
