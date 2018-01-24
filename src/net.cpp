ac86e74827d8c9df562e597e286992b44e26bcff
FranckDG <franck@codeparticle.com>
2018-01-24 14:55:57 -0800

    Integrated Commit: Stay non-blocking for SSL_read #0836b27e

--- src/net.cpp
+++ src/net.cpp
@@ -1536,22 +1536,10 @@ void ThreadSocketHandler()
                         {
 // ZEN_MOD_START
                             if (ssl_err == SSL_ERROR_WANT_READ) {
-                                fd_set fds;
-                                int sock = SSL_get_rfd(pnode->ssl);
-                                FD_ZERO(&fds);
-                                FD_SET(sock, &fds);
-                                int sel = select(sock+1, &fds, NULL, NULL, &timeout);
-                                boost::this_thread::interruption_point();
-                                repeat = true;
+                                repeat = false;
                             }
                             else if (ssl_err == SSL_ERROR_WANT_WRITE) {
-                                fd_set fds;
-                                int sock = SSL_get_wfd(pnode->ssl);
-                                FD_ZERO(&fds);
-                                FD_SET(sock, &fds);
-                                int sel = select(sock+1, NULL, &fds, NULL, &timeout);
-                                boost::this_thread::interruption_point();
-                                repeat = true;
+                                repeat = false;
                             }
                             else if (ssl_err == SSL_ERROR_WANT_CONNECT) {
                                 SSL_connect(pnode->ssl);
