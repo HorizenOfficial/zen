5b8feaa49a8e817b1b5aea49269def4c3dafdc11
FranckDG <franck@codeparticle.com>
2018-01-24 15:30:25 -0800

    Integrated Commit: Update default port for ZEN #d888a776

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -69,8 +69,8 @@ public:
         pchMessageStart[2] = 0x73;
         pchMessageStart[3] = 0x68;
         vAlertPubKey = ParseHex("048679fb891b15d0cada9692047fd0ae26ad8bfb83fabddbb50334ee5bc0683294deb410be20513c5af6e7b9cec717ade82b27080ee6ef9a245c36a795ab044bb3");
-        nDefaultPort = 8033;
-//        nMinerThreads = 0;
+        nDefaultPort = 9033;
+        nMinerThreads = 0;
         std::time_t result = std::time(nullptr);
         if (result < consensus.nChainsplitTime + (24 * 60 * 60))
             nMaxTipAge = 10 * 24 * 60 * 60; // Allow blocks up to 10 days old to be the chain tip
