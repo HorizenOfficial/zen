cb02a4a695dbbb90be19f885ebcff7b547921203
FranckDG <franck@codeparticle.com>
2018-01-26 16:23:57 -0800

    Integrated Commit: Set new multisig founders reward address for testnet #a2b197df

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -367,7 +367,7 @@ public:
         // vFoundersRewardAddress2 is another set of FR addresses that substitutes old set since hfFoundersRewardsHeight block
         // TODO: change with real addresses
         vFoundersRewardAddress2 = {
-            "zrCsWfwKotWnQmFviqAHAPAJ2jXqZYW966P", "zrLLB3JB3jozUoMGFEGhjqyVXTpngVQ8c4T", "zrAEa8YjJ2f3m2VsM1Xa9EwibZxEnRoSLUx", "zrAdJgp7Cx35xTvB7ABWP8YLTNDArMjP1s3"
+            "zrRBQ5heytPMN5nY3ssPf3cG4jocXeD8fm1"
         };
         assert(vFoundersRewardAddress.size() <= consensus.GetLastFoundersRewardBlockHeight());
         assert(vFoundersRewardAddress2.size() <= consensus.GetLastFoundersRewardBlockHeight() - consensus.hfFoundersRewardHeight - 1);
