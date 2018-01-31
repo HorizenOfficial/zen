37952571d8519e60fe70f5ec782d69f9c2272385
FranckDG <franck@codeparticle.com>
2018-01-31 09:43:52 -0800

    Integrated Commit: TLS: add tls certificate status to getpeerinfo and getnetworkinfo #5ef497d7

--- src/net.cpp
+++ src/net.cpp
@@ -118,7 +118,7 @@ CNodeSignals& GetNodeSignals() { return g_signals; }
 
 // ZEN_MOD_START
 // OpenSSL server and client contexts
-static SSL_CTX *tls_ctx_server, *tls_ctx_client;
+SSL_CTX *tls_ctx_server, *tls_ctx_client;
 
 typedef struct _NODE_ADDR
 {
@@ -825,6 +825,7 @@ void CNode::copyStats(CNodeStats &stats)
     {
         LOCK(cs_hSocket);
         stats.fTLSEstablished = (ssl != NULL) && (SSL_get_state(ssl) == TLS_ST_OK);
+        stats.fTLSVerified = (ssl != NULL) && ValidatePeerCertificate(ssl);
     }
 // ZEN_MOD_END
 }
