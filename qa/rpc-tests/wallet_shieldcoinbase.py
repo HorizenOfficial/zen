321e14d45cafa8735935ffc5d89f3d2d6d1af088
FranckDG <franck@codeparticle.com>
2018-02-01 10:50:28 -0800

    Integrated Commit: Closes #2639. Adds optional limit parameter with a default value of 50. #5fbdc0c9

--- qa/rpc-tests/wallet_shieldcoinbase.py
+++ qa/rpc-tests/wallet_shieldcoinbase.py
@@ -202,5 +202,23 @@ class WalletShieldCoinbaseTest (BitcoinTestFramework):
         self.nodes[1].generate(1)
         self.sync_all()
 
+        # Verify maximum number of utxos which node 0 can shield is set by default limit parameter of 50
+        self.nodes[0].generate(200)
+        self.sync_all()
+        mytaddr = self.nodes[0].getnewaddress()
+        result = self.nodes[0].z_shieldcoinbase(mytaddr, myzaddr, Decimal('0.0001'))
+        assert_equal(result["shieldingUTXOs"], Decimal('50'))
+        assert_equal(result["remainingUTXOs"], Decimal('50'))
+        self.wait_and_assert_operationid_status(0, result['opid'])
+
+        # Verify maximum number of utxos which node 0 can shield can be set by the limit parameter
+        result = self.nodes[0].z_shieldcoinbase(mytaddr, myzaddr, Decimal('0.0001'), 33)
+        assert_equal(result["shieldingUTXOs"], Decimal('33'))
+        assert_equal(result["remainingUTXOs"], Decimal('17'))
+        self.wait_and_assert_operationid_status(0, result['opid'])
+        sync_blocks(self.nodes)  # don't sync on mempool as node 2 will reject thix tx due to its mempooltxinputlimit
+        self.nodes[1].generate(1)
+        self.sync_all()
+
 if __name__ == '__main__':
     WalletShieldCoinbaseTest().main()
