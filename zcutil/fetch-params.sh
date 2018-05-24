843623f4aa7b682343990444e0ef17336420b8f4
cronicc <cronic@zensystem.io>
2018-05-24 12:26:05 +0000

    Fix fetch-params.sh

--- zcutil/fetch-params.sh
+++ zcutil/fetch-params.sh
@@ -7,9 +7,9 @@ PARAMS_DIR="$HOME/.zcash-params"
 SPROUT_PKEY_NAME='sprout-proving.key'
 SPROUT_VKEY_NAME='sprout-verifying.key'
 # ZEN_MOD_START
-SPROUT_PKEY_URL="https://zensystem.io/downloads/$SPROUT_PKEY_NAME"
-SPROUT_VKEY_URL="https://zensystem.io/downloads/$SPROUT_VKEY_NAME"
+SPROUT_URL="https://zensystem.io/downloads"
 # ZEN_MOD_END
+SPROUT_IPFS="/ipfs/QmZKKx7Xup7LiAtFRhYsE1M7waXcv9ir9eCECyXAFGxhEo"
 
 SHA256CMD="$(command -v sha256sum || echo shasum)"
 SHA256ARGS="$(command -v sha256sum >/dev/null || echo '-a 256')"
