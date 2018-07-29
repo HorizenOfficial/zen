f0131f16c95a89647ed5448dc084ec15f249ba42
hellcatz <kg6ypi@remotehams.com>
2018-07-29 12:22:40 -0700

    Update walletdb.cpp

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -920,7 +920,7 @@ DBErrors CWalletDB::ZapWalletTx(CWallet* pwallet, vector<CWalletTx>& vWtx)
 void ThreadFlushWalletDB(const string& strFile)
 {
     // Make this thread recognisable as the wallet flushing thread
-    RenameThread("zcash-wallet");
+    RenameThread("zen-wallet");
 
     static bool fOneThread;
     if (fOneThread)
