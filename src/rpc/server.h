726cfab9ed282be9e3688f8600fcf8e056cf9feb
Alberto Sala <alberto.sala@nttdata.com>
2019-03-22 17:51:23 +0100

    Added rpc method getglobaltips

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -269,6 +269,7 @@ extern UniValue getblockhash(const UniValue& params, bool fHelp);
 extern UniValue getblockheader(const UniValue& params, bool fHelp);
 extern UniValue getblock(const UniValue& params, bool fHelp);
 extern UniValue getblockfinalityindex(const UniValue& params, bool fHelp);
+extern UniValue getglobaltips(const UniValue& params, bool fHelp);
 extern UniValue gettxoutsetinfo(const UniValue& params, bool fHelp);
 extern UniValue gettxout(const UniValue& params, bool fHelp);
 extern UniValue verifychain(const UniValue& params, bool fHelp);
