2e88bb535ea93d1c3a70e268c7dc54b8a0f73d13
Alberto Sala <alberto.sala@nttdata.com>
2019-03-25 17:14:37 +0100

    Removed debug log categories from zen.conf in py tests; minor modification to blockdelay py test; minor files cleanup

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -50,16 +50,6 @@ testScripts=(
     'bipdersig-p2p.py'
     'nulldata.py'
     'blockdelay.py'
-    'headers_01.py'
-    'headers_02.py'
-    'headers_03.py'
-    'headers_04.py'
-    'headers_05.py'
-    'headers_06.py'
-    'headers_07.py'
-    'headers_08.py'
-    'headers_09.py'
-    'headers_10.py'
 );
 testScriptsExt=(
     'getblocktemplate_longpoll.py'
--- qa/rpc-tests/test_framework/util.py
+++ qa/rpc-tests/test_framework/util.py
@@ -96,17 +96,8 @@ def initialize_datadir(dirname, n):
         f.write("port="+str(p2p_port(n))+"\n");
         f.write("rpcport="+str(rpc_port(n))+"\n");
         f.write("listenonion=0\n");
-        f.write("debug=forks\n");
-        f.write("debug=status\n");
-        f.write("debug=forks_2\n");
-        f.write("debug=py\n");
-        f.write("debug=net\n");
-#        f.write("debug=bench\n");
-#        f.write("debug=rpc\n");
-#        f.write("debug=alert\n");
-#        f.write("debug=http\n");
-        f.write("logtimemicros=1\n");
-#        f.write("latestblockscapacity=100\n");
+#        f.write("debug=net\n");
+#        f.write("logtimemicros=1\n");
     return datadir
 
 def initialize_chain(test_dir):
