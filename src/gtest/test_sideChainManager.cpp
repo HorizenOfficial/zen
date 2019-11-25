8de8b061c3e14b9fa541166b4ed4d515b82ccc20
abi87 <alberto.benegiamo@gmail.com>
2019-11-25 12:56:43 +0100

    minor improvement of BaseChainParams reset

--- src/gtest/test_sideChainManager.cpp
+++ src/gtest/test_sideChainManager.cpp
@@ -1,44 +1,52 @@
 #include <gtest/gtest.h>
 #include <sc/sidechain.h>
 #include <chainparamsbase.h>
-#include <chainparamsbase.cpp>
 
 class ScManagerTestSuite: public ::testing::Test {
 
 public:
 	ScManagerTestSuite() :
-			rSideChainManager(Sidechain::ScMgr::instance()) {
+			sideChainManager(Sidechain::ScMgr::instance()) {
 	};
 
 	~ScManagerTestSuite() {
-		rSideChainManager.reset();
+		sideChainManager.reset();
 	};
 
 	void SetUp() override {
 		SelectBaseParams(CBaseChainParams::TESTNET);
-		ASSERT_TRUE(pCurrentBaseParams != nullptr)<<"Chain type must be specified before actual tests start";
 	};
 
 	void TearDown() override {
-		pCurrentBaseParams = nullptr; //global variable, which needs to be reset test by test
+		//This means that at the exit of current test suite, following test will have to setup BaseParams again
+		resetBaseParams();
 	};
 
 protected:
-	Sidechain::ScMgr& rSideChainManager;
+	Sidechain::ScMgr&           sideChainManager;
+	Sidechain::ScCoinsViewCache coinViewCache;
+
+	//TODO: Consider storing initial CBaseChainParam and reset it upon TearDown; try and handle assert
+	//TODO: evaluate moving resetBaseParams to chainparamsbase.h
+	void resetBaseParams() {
+		//force reset of pCurrentBaseParams, very ugly way
+		CBaseChainParams* nakedCurrentBaseParams = &const_cast <CBaseChainParams &>(BaseParams());
+		nakedCurrentBaseParams = nullptr;
+	}
 };
 
 TEST_F(ScManagerTestSuite, ManagerIsSingleton) {
 	Sidechain::ScMgr& rAnotherScMgrInstance = Sidechain::ScMgr::instance();
-	EXPECT_TRUE(&rSideChainManager == &rAnotherScMgrInstance)
+	EXPECT_TRUE(&sideChainManager == &rAnotherScMgrInstance)
 			<< "ScManager Instances have different address:"
-			<< &rSideChainManager << " and " << &rAnotherScMgrInstance;
+			<< &sideChainManager << " and " << &rAnotherScMgrInstance;
 }
 
 TEST_F(ScManagerTestSuite, InitCanBePerformedWithZeroCacheAndWipe) {
 
 	size_t cacheSize(0);
 	bool fWipe(false);
-	bool bRet = rSideChainManager.initialUpdateFromDb(cacheSize, fWipe);
+	bool bRet = sideChainManager.initialUpdateFromDb(cacheSize, fWipe);
 
 	EXPECT_TRUE(bRet) << "Db initialization failed";
 	//not sure yet how to double check db creation/availability
@@ -47,8 +55,8 @@ TEST_F(ScManagerTestSuite, InitCanBePerformedWithZeroCacheAndWipe) {
 TEST_F(ScManagerTestSuite, DoubleInitializationIsForbidden) {
 	size_t cacheSize(0);
 	bool fWipe(false);
-	ASSERT_TRUE(rSideChainManager.initialUpdateFromDb(cacheSize, fWipe))<<"Db first initialization should succeed";
+	ASSERT_TRUE(sideChainManager.initialUpdateFromDb(cacheSize, fWipe))<<"Db first initialization should succeed";
 
-	bool bRet = rSideChainManager.initialUpdateFromDb(cacheSize, fWipe);
+	bool bRet = sideChainManager.initialUpdateFromDb(cacheSize, fWipe);
 	EXPECT_FALSE(bRet) << "Db double initialization should be forbidden";
 }
