b104f7e2c92ab466cbb80b640c576678cacdb9fe
abi87 <alberto.benegiamo@gmail.com>
2019-12-11 11:49:36 +0100

    minor gtest cleanup

--- src/wallet/gtest/test_deadlock.cpp
+++ src/wallet/gtest/test_deadlock.cpp
@@ -14,84 +14,74 @@ using namespace libzcash;
 
 extern ZCJoinSplit *params;
 
+void write_db(CWallet &wallet, CBlock &block, SpendingKey &sk, std::atomic_int &finish)
+{
+    for (int i = 0; i < 1000; i++) {
+        auto wtx = GetValidReceive(*params, sk, 10, true);
+        wallet.SyncTransaction(wtx, &block);
+    }
 
-CWalletTx GetValidTx(const libzcash::SpendingKey &sk, CAmount value,
-		bool randomInputs) {
-	return GetValidReceive(*params, sk, value, randomInputs);
-}
-
-
-TEST(deadlock_test, setup_datadir_location_run_as_first_test) {
-	// Get temporary and unique path for file.
-	boost::filesystem::path pathTemp = boost::filesystem::temp_directory_path()
-			/ boost::filesystem::unique_path();
-	boost::filesystem::create_directories(pathTemp);
-	mapArgs["-datadir"] = pathTemp.string();
-}
-
-void write_db(CWallet &wallet, CBlock &block, SpendingKey &sk,
-		std::atomic_int &finish) {
-
-	for (int i = 0; i < 1000; i++) {
-		auto wtx = GetValidTx(sk, 10, true);
-
-		wallet.SyncTransaction(wtx, &block);
-	}
-
-	finish++;
+    finish++;
 }
 
 void write_block(CWallet &walletdb, std::atomic_int &finish) {
+    for (int i = 0; i < 100; i++) {
+        CBlockLocator loc;
+        walletdb.SetBestChain(loc);
+    }
 
-	for (int i = 0; i < 100; i++) {
-		CBlockLocator loc;
-		walletdb.SetBestChain(loc);
-
-	}
-	finish++;
+    finish++;
 }
 
 TEST(deadlock_test, deadlock) {
+    std::string originalDataDir("");
+    if (mapArgs.count("-datadir") != 0)
+        originalDataDir = mapArgs["-datadir"];
 
-	//create wallet
-	SelectParams(CBaseChainParams::TESTNET);
-	bool fFirstRun = true;
-	CWallet pwalletMain("wallet.dat");
-	DBErrors nLoadWalletRet = pwalletMain.LoadWallet(fFirstRun);
+    //set tmp db folder
+    boost::filesystem::path pathTemp = boost::filesystem::temp_directory_path()
+            / boost::filesystem::unique_path();
+    boost::filesystem::create_directories(pathTemp);
+    mapArgs["-datadir"] = pathTemp.string();
 
-	auto sk = libzcash::SpendingKey::random();
-	pwalletMain.AddSpendingKey(sk);
+    //create wallet
+    SelectParams(CBaseChainParams::TESTNET);
+    bool fFirstRun = true;
+    CWallet walletMain("deadlock_ut_wallet.dat");
+    DBErrors nLoadWalletRet = walletMain.LoadWallet(fFirstRun);
 
-	//create block
-	CBlock block;
+    auto sk = libzcash::SpendingKey::random();
+    walletMain.AddSpendingKey(sk);
 
-	//number of concurrent SyncTransaction Thread -1
-	int size = 2;
-	std::atomic_int finished(0);
-	std::thread myThreads[size];
+    //create block
+    CBlock block;
 
-	//launch #size -1 thread to process transaction
-	for (int i = 0; i < size; i++) {
+    //number of concurrent SyncTransaction Thread -1
+    int size = 2;
+    std::atomic_int finished(0);
+    std::thread myThreads[size];
 
-		if (i == 0) {
-			myThreads[i] = std::thread(write_block, std::ref(pwalletMain),
-					std::ref(finished));
+    //launch #size -1 thread to process transaction
+    for (int i = 0; i < size; i++) {
+        if (i == 0) {
+            myThreads[i] = std::thread(write_block, std::ref(walletMain),
+                    std::ref(finished));
 
-		}
-		//otherwise it process txes
-		else {
-			myThreads[i] = std::thread(write_db, std::ref(pwalletMain),
-					std::ref(block), std::ref(sk), std::ref(finished));
+        }
+        else { //otherwise it process txes
+            myThreads[i] = std::thread(write_db, std::ref(walletMain),
+                    std::ref(block), std::ref(sk), std::ref(finished));
+        }
 
-		}
+    }
 
-	}
+    //wait all threads to finish
+    for (int i = 0; i < size; i++) {
+        myThreads[i].join();
+    }
 
-	//wait all threads to finish
-	for (int i = 0; i < size; i++) {
-		myThreads[i].join();
-	}
+    EXPECT_EQ(finished, size);
 
-	EXPECT_EQ(finished, size);
-
+    if (!originalDataDir.empty())
+        mapArgs["-datadir"] = originalDataDir;
 }
