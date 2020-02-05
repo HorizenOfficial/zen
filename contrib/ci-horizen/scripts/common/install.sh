1410ddfe83579288c64997399d6afb5f7a66ff5f
cronicc <cronic@zensystem.io>
2020-02-05 15:54:06 +0000

    Add travis-ci CI/CD pipeline part1

---
+++ contrib/ci-horizen/scripts/common/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  if [ ! -z "${DOCKER_UPDATE_PACKAGES}" ]; then
+    sudo add-apt-repository "deb [arch=${TRAVIS_CPU_ARCH}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    UPDATE_PACKAGES="${UPDATE_PACKAGES} ${DOCKER_UPDATE_PACKAGES}"
+  fi
+  if [ ! -z "${PIP_UPDATE_PACKAGES}" ] && [ ! -z "${PIP_INSTALL}" ]; then
+    UPDATE_PACKAGES="${UPDATE_PACKAGES} ${PIP_UPDATE_PACKAGES}"
+  fi
+  sudo apt-get update
+  sudo apt-get -y --no-install-recommends install ${UPDATE_PACKAGES}
+  if [ ! -z "${DOCKER_UPDATE_PACKAGES}" ]; then
+    ls /proc/sys/fs/binfmt_misc/
+    if [ "${TRAVIS_CPU_ARCH}" = "amd64" ]; then
+      docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+      docker image rm multiarch/qemu-user-static:latest
+    fi
+  fi
+  if [ ! -z "${PIP_INSTALL}" ]; then
+    sudo pip install --upgrade ${PIP_INSTALL}
+  fi
+fi
+
+if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+  brew install ${UPDATE_PACKAGES}
+  if [ ! -z "${PIP_INSTALL}" ]; then
+    sudo pip install --upgrade ${PIP_INSTALL}
+  fi
+fi
