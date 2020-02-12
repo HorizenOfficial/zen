ad6462fc19c8c7eaf9f73ecf13ff8d278309215c
abi87 <alberto.benegiamo@gmail.com>
2020-02-12 11:56:18 +0100

    Added extra check on alsa's python test

--- qa/rpc-tests/sc_conflicting_creation.py
+++ qa/rpc-tests/sc_conflicting_creation.py
@@ -7,7 +7,7 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.authproxy import JSONRPCException
 from test_framework.util import assert_equal, initialize_chain_clean, \
     start_nodes, sync_blocks, sync_mempools, connect_nodes_bi, p2p_port, \
-    mark_logs
+    mark_logs, disconnect_nodes
 import os
 from decimal import Decimal
 import time
@@ -44,19 +44,11 @@ class ScConflictingCreation(BitcoinTestFramework):
         self.is_network_split = split
         self.sync_all()
 
-    def disconnect_nodes(self, from_connection, node_num):
-        ip_port = "127.0.0.1:" + str(p2p_port(node_num))
-        from_connection.disconnectnode(ip_port)
-        # poll until version handshake complete to avoid race conditions
-        # with transaction relaying
-        while any(peer['version'] == 0 for peer in from_connection.getpeerinfo()):
-            time.sleep(0.1)
-
     def split_network(self):
         # Split the network of two nodes into nodes 0 and 1.
         assert not self.is_network_split
-        self.disconnect_nodes(self.nodes[0], 1)
-        self.disconnect_nodes(self.nodes[1], 0)
+        disconnect_nodes(self.nodes[0], 1)
+        disconnect_nodes(self.nodes[1], 0)
         self.is_network_split = True
 
     def join_network(self):
@@ -69,9 +61,11 @@ class ScConflictingCreation(BitcoinTestFramework):
 
     def run_test(self):
         '''
-        This test try to create the same SC and forward funds in two nodes which are disjointed
-        The network is then joined and mining takes place. At the end, one of the creation tx is removed from mempool and 
-        all the funds are gathered in the persisted SC
+        This test shows how a SC creation tx race through the network is handled.
+        While different txs declaring the same SC can exist in two nodes' mempools,
+        the mining process solve the duplication confirming one the SC creation tx
+        and removing the conflicting one from mempools.
+        Fwd txs are not affected by the duplication cleanup and are duly confirmed.
         '''
         # network topology: (0)-(1)
         scid = "22"
@@ -92,116 +86,127 @@ class ScConflictingCreation(BitcoinTestFramework):
         self.nodes[1].generate(110)
         self.sync_all()
 
-        tx0 = []
-        tx0_f = []
-
-        tx1 = []
-        tx1_f = []
-
-        # Split the network: (0)--(1) / (2)
+        # Split the network: (0) / (1)
         mark_logs("\nSplit network", self.nodes,DEBUG_MODE)
         self.split_network()
-        mark_logs("The network is split: 0 .. 1", self.nodes,DEBUG_MODE)
+        mark_logs("The network is split: 0 / 1", self.nodes,DEBUG_MODE)
 
         mark_logs("\nNode 0 create a SC...", self.nodes, DEBUG_MODE)
         try:
-            tx0 = self.nodes[0].sc_create(scid, 123, amounts_0)
-            if DEBUG_MODE != 0: print "tx0   = ", tx0
+            sc_by_0 = self.nodes[0].sc_create(scid, 123, amounts_0)
+            if DEBUG_MODE != 0: print "sc_by_0   = ", sc_by_0
         except JSONRPCException, e:
             errorString = e.error['message']
             if DEBUG_MODE != 0: print (errorString)
 
         mark_logs("\nNode 0 send funds to the SC...", self.nodes, DEBUG_MODE)
         try:
-            tx0_f = self.nodes[0].sc_send("abcd", fwt_amount_0, scid)
-            if DEBUG_MODE != 0: print "tx0_f = ", tx0_f
+            fwt_by_0 = self.nodes[0].sc_send("abcd", fwt_amount_0, scid)
+            if DEBUG_MODE != 0: print "fwt_by_0 = ", fwt_by_0
         except JSONRPCException, e:
             errorString = e.error['message']
-            if DEBUG_MODE != 0: print (errorString)
+            if DEBUG_MODE != 0: print (errorString)        
 
         mark_logs("\nNode 1 create a SC using the same scid...", self.nodes, DEBUG_MODE)
         try:
-            tx1 = self.nodes[1].sc_create(scid, 123, amounts_1)
-            if DEBUG_MODE != 0: print "tx1   = ", tx1
+            sc_by_1 = self.nodes[1].sc_create(scid, 123, amounts_1)
+            if DEBUG_MODE != 0: print "sc_by_1   = ", sc_by_1
         except JSONRPCException, e:
             errorString = e.error['message']
             if DEBUG_MODE != 0: print (errorString)
 
         mark_logs("\nNode 0 send funds to the SC...", self.nodes, DEBUG_MODE)
         try:
-            tx1_f = self.nodes[1].sc_send("abcd", fwt_amount_1, scid)
-            if DEBUG_MODE != 0: print "tx1_f = ", tx1_f
+            fwt_by_1 = self.nodes[1].sc_send("abcd", fwt_amount_1, scid)
+            if DEBUG_MODE != 0: print "fwt_by_1 = ", fwt_by_1
         except JSONRPCException, e:
             errorString = e.error['message']
             if DEBUG_MODE != 0: print (errorString)
-
-        sync_blocks(self.nodes[1:2])
-        sync_mempools(self.nodes[1:2])
-        sync_blocks(self.nodes[0:1])
-        sync_mempools(self.nodes[0:1])
 
         mark_logs("\nJoining network", self.nodes,DEBUG_MODE)
         self.join_network()
         mark_logs("Network joined", self.nodes,DEBUG_MODE)
 
-        # txes pair are in the proper mempool
+        # Mempools tell different candidate histories
         mark_logs("Check that txes pair are in the proper mempool", self.nodes,DEBUG_MODE)
-        assert_equal(tx0 in self.nodes[0].getrawmempool(), True)
-        assert_equal(tx0_f in self.nodes[0].getrawmempool(), True)
-        assert_equal(tx1 in self.nodes[1].getrawmempool(), True)
-        assert_equal(tx1_f in self.nodes[1].getrawmempool(), True)
+        assert_equal(sc_by_0 in self.nodes[0].getrawmempool(), True)
+        assert_equal(fwt_by_0 in self.nodes[0].getrawmempool(), True)
+        assert_equal(sc_by_1 in self.nodes[0].getrawmempool(), False)
+        assert_equal(fwt_by_1 in self.nodes[0].getrawmempool(), False)
+        
+        assert_equal(sc_by_1 in self.nodes[1].getrawmempool(), True)
+        assert_equal(fwt_by_1 in self.nodes[1].getrawmempool(), True)
+        assert_equal(sc_by_0 in self.nodes[1].getrawmempool(), False)
+        assert_equal(fwt_by_0 in self.nodes[1].getrawmempool(), False)
         if DEBUG_MODE != 0: print ("...OK")
 
         mark_logs("Node 1 generates 1 block", self.nodes,DEBUG_MODE)
         self.nodes[1].generate(1)
         sync_blocks(self.nodes)
 
-        # fw txes are in the proper mempool
-        mark_logs("Check that each fw tx is in its node mempool", self.nodes,DEBUG_MODE)
-        assert_equal(tx0_f in self.nodes[0].getrawmempool(), True)
-        assert_equal(tx1_f in self.nodes[1].getrawmempool(), True)
-        if DEBUG_MODE != 0: print ("...OK")
-        # creations are not
-        mark_logs("Check that both creation txes have been removed from node mempool", self.nodes,DEBUG_MODE)
-        assert_equal(tx0 in self.nodes[0].getrawmempool(), False)
-        assert_equal(tx1 in self.nodes[1].getrawmempool(), False)
+        # Once a block is mined, creation from node1 is accepted by node0
+        mark_logs("Check that creation tx from Node 1 is accepted into Node0's chain best block", self.nodes,DEBUG_MODE)
+        assert_equal(sc_by_1 in self.nodes[0].getblock(self.nodes[0].getbestblockhash(), True)['tx'], True )
         if DEBUG_MODE != 0: print ("...OK")
 
-        # but creation from node1 is in the best block (check node0)
-        mark_logs("Check that creation tx from Node 1 is in the chain best block", self.nodes,DEBUG_MODE)
-        assert_equal(tx1 in self.nodes[0].getblock(self.nodes[0].getbestblockhash(), True)['tx'], True )
-        if DEBUG_MODE != 0: print ("...OK")
-        # while creation from node0 is not in the best block either
-        mark_logs("Check that creation tx from Node 0 is NOT in the chain best block", self.nodes,DEBUG_MODE)
-        assert_equal(tx0 in self.nodes[0].getblock(self.nodes[0].getbestblockhash(), True)['tx'], False )
+        # Both nodes see the Sc created by creation tx coming from node 1
+        mark_logs("Check that both Nodes see SC created by tx from Node1", self.nodes,DEBUG_MODE)
+        print(self.nodes[0].getscinfo(scid))
+        assert_equal(sc_by_1, self.nodes[0].getscinfo(scid)['creating tx hash'])
+        assert_equal(sc_by_1, self.nodes[1].getscinfo(scid)['creating tx hash'])
         if DEBUG_MODE != 0: print ("...OK")
 
+        # both SC creation txs are removed from the mempools
+        mark_logs("Check that both creation txes have been removed from the mempools", self.nodes,DEBUG_MODE)
+        assert_equal(sc_by_0 in self.nodes[0].getrawmempool(), False)        
+        assert_equal(sc_by_0 in self.nodes[1].getrawmempool(), False)
+        assert_equal(sc_by_1 in self.nodes[0].getrawmempool(), False)
+        assert_equal(sc_by_1 in self.nodes[1].getrawmempool(), False)
+        if DEBUG_MODE != 0: print ("...OK")
+
+        # while fwd txs are still in their mempools
+        mark_logs("Check that each fwd txs are in their node mempools", self.nodes,DEBUG_MODE)
+        assert_equal(fwt_by_0 in self.nodes[0].getrawmempool(), True)
+        assert_equal(fwt_by_1 in self.nodes[0].getrawmempool(), False)
+        assert_equal(fwt_by_0 in self.nodes[1].getrawmempool(), False)
+        assert_equal(fwt_by_1 in self.nodes[1].getrawmempool(), True)
+        if DEBUG_MODE != 0: print ("...OK")
+
+        # next block mined by Node1 confirms fwt_by_1
         mark_logs("Node 1 generates 1 block", self.nodes,DEBUG_MODE)
         self.nodes[1].generate(1)
         sync_blocks(self.nodes)
 
-        # fw tx of node0 is still in mempool 
-        mark_logs("Check that fw tx from Node0 is still in its node mempool", self.nodes,DEBUG_MODE)
-        assert_equal(tx0_f in self.nodes[0].getrawmempool(), True)
+        # Once a block is mined, fwd tx from node1 is accepted by node0
+        mark_logs("Check that fwd tx from Node 1 is not in Node0's chain best block", self.nodes,DEBUG_MODE)
+        assert_equal(fwt_by_1 in self.nodes[0].getblock(self.nodes[0].getbestblockhash(), True)['tx'], True ) 
         if DEBUG_MODE != 0: print ("...OK")
-        # node1 mempool is empty now
+
+        # fwd tx of node0 is still in mempool
+        mark_logs("Check that fwd tx from Node0 is still in its node mempool", self.nodes,DEBUG_MODE)
+        assert_equal(fwt_by_0 in self.nodes[0].getrawmempool(), True)
+        if DEBUG_MODE != 0: print ("...OK")
+
+        # ...while node1 mempool is empty now
         mark_logs("Check that now Node1 has an empty mempool", self.nodes,DEBUG_MODE)
         assert_equal(len(self.nodes[1].getrawmempool()), 0)
         if DEBUG_MODE != 0: print ("...OK")
 
+        # next block mined by Node0 confirms fwt_by_0
         mark_logs("Node 0 generates 1 block", self.nodes,DEBUG_MODE)
         self.nodes[0].generate(1)
         self.sync_all()
+
+        # Once a block is mined, fwd tx from node0 is accepted by node1
+        mark_logs("Check that fwd tx from Node 0 is in Node1's chain best block", self.nodes,DEBUG_MODE)
+        assert_equal(fwt_by_0 in self.nodes[1].getblock(self.nodes[1].getbestblockhash(), True)['tx'], True )
+        if DEBUG_MODE != 0: print ("...OK")
 
         # node0 mempool is also empty now
         mark_logs("Check that now also Node0 has an empty mempool", self.nodes,DEBUG_MODE)
         assert_equal(len(self.nodes[0].getrawmempool()), 0)
         if DEBUG_MODE != 0: print ("...OK")
-        # and block has it (check node 1)
-        mark_logs("Check that fw tx from Node 0 is in the chain best block", self.nodes,DEBUG_MODE)
-        assert_equal(tx0_f in self.nodes[1].getblock(self.nodes[1].getbestblockhash(), True)['tx'], True )
-        if DEBUG_MODE != 0: print ("...OK")
-        
+
         # both nodes have the same view of the sc 
         mark_logs("Check that both Nodes sees the same sc info", self.nodes,DEBUG_MODE)
         assert_equal(self.nodes[0].getscinfo(scid), self.nodes[0].getscinfo(scid))
