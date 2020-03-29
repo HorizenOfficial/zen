13617614038c4144895f6dae2ea2f098c96bf709
abi87 <alberto.benegiamo@gmail.com>
2020-03-29 18:11:55 +0200

    Refactored isStandardTx to remove code duplication.

--- src/primitives/certificate.h
+++ src/primitives/certificate.h
@@ -74,6 +74,7 @@ public:
 
     //CHECK FUNCTIONS
     bool CheckVersionBasic        (CValidationState &state) const override;
+    bool CheckVersionIsStandard   (std::string& reason, const int nHeight) const override;
     bool CheckInputsAvailability  (CValidationState &state) const override;
     bool CheckOutputsAvailability (CValidationState &state) const override;
     bool CheckSerializedSize      (CValidationState &state) const override;
@@ -108,8 +109,6 @@ public:
     bool ContextualCheck(CValidationState& state, int nHeight, int dosLevel) const override;
     bool CheckFinal(int flags) const override;
     bool IsApplicableToState(CValidationState& state, int nHeight = -1) const override;
-
-    bool IsStandard(std::string& reason, int nHeight) const override;
 
     bool TryPushToMempool(bool fLimitFree, bool fRejectAbsurdFee) override final;
 
