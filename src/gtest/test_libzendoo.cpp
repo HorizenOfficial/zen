94c3463e6f9ff31b8aaa6f6df7f8bc96b130586f
DanieleDiBenedetto <daniele@horizenlabs.io>
2020-08-27 12:21:56 -0400

    Updated zendoo tests

--- src/gtest/test_libzendoo.cpp
+++ src/gtest/test_libzendoo.cpp
@@ -58,61 +58,79 @@ TEST(ZendooLib, PoseidonHashTest)
     auto rhs_field = zendoo_deserialize_field(rhs);
     ASSERT_TRUE(rhs_field != NULL);
 
+
     auto expected_hash = zendoo_deserialize_field(hash);
     ASSERT_TRUE(expected_hash != NULL);
 
-    const field_t* hash_input[] = {lhs_field, rhs_field};
+    auto digest = ZendooPoseidonHash(NULL, 0);
 
-    auto actual_hash = zendoo_compute_poseidon_hash(hash_input, 2);
-    ASSERT_TRUE(actual_hash != NULL);
+    digest.update(lhs_field);
 
-    ASSERT_TRUE(zendoo_field_assert_eq(expected_hash, actual_hash));
+    auto temp_hash = digest.finalize();
+    digest.update(rhs_field); // Call to finalize keeps the state
 
+    auto actual_hash = digest.finalize();
+    ASSERT_TRUE(("Expected hashes to be equal", zendoo_field_assert_eq(actual_hash, expected_hash)));
+    zendoo_field_free(actual_hash);
+
+    auto actual_hash_2 = digest.finalize(); // finalize() is idempotent
+    ASSERT_TRUE(("Expected hashes to be equal", zendoo_field_assert_eq(actual_hash_2, expected_hash)));
+    zendoo_field_free(actual_hash_2);
+
+    zendoo_field_free(expected_hash);
+    zendoo_field_free(temp_hash);
     zendoo_field_free(lhs_field);
     zendoo_field_free(rhs_field);
-    zendoo_field_free(expected_hash);
-    zendoo_field_free(actual_hash);
 }
 
 TEST(ZendooLib, PoseidonMerkleTreeTest)  {
 
-    //Generate random leaves
-    int leaves_len = 16;
+    size_t height = 10;
+
+    // Deserialize root
+    unsigned char expected_root_bytes[96] = {
+        231, 64, 42, 251, 206, 22, 102, 105, 222, 145, 252, 133, 62, 169, 60, 150, 50, 133, 187, 38, 47, 246, 192, 170,
+        161, 204, 152, 177, 20, 209, 217, 101, 34, 159, 246, 176, 23, 223, 62, 191, 103, 165, 210, 114, 179, 110, 140,
+        252, 250, 167, 106, 31, 7, 178, 109, 108, 20, 239, 162, 121, 99, 207, 137, 224, 124, 212, 65, 229, 5, 112, 116,
+        75, 145, 11, 77, 252, 134, 37, 127, 54, 244, 236, 68, 129, 16, 191, 196, 6, 17, 185, 138, 98, 183, 153, 1, 0
+    };
+    auto expected_root = zendoo_deserialize_field(expected_root_bytes);
+    ASSERT_TRUE(expected_root != NULL);
+
+    //Generate leaves
+    int leaves_len = 512;
     const field_t* leaves[leaves_len];
     for (int i = 0; i < leaves_len; i++){
-        leaves[i] = zendoo_get_random_field();
+        leaves[i] = zendoo_get_field_from_long(i);
     }
 
-    //Create Merkle Tree and get the root
-    auto tree = ginger_mt_new(leaves, leaves_len);
-    ASSERT_TRUE(tree != NULL);
+    // Initialize tree
+    auto tree = ZendooGingerRandomAccessMerkleTree(height);
 
-    auto root = ginger_mt_get_root(tree);
-
-    //Verify Merkle Path is ok for each leaf
-    for (int i = 0; i < leaves_len; i++) {
-
-        //Create Merkle Path for the i-th leaf
-        auto path = ginger_mt_get_merkle_path(leaves[i], i, tree);
-        ASSERT_TRUE(path != NULL);
-
-        //Verify Merkle Path for the i-th leaf
-        ASSERT_TRUE(ginger_mt_verify_merkle_path(leaves[i], root, path));
-
-        //Free Merkle Path
-        ginger_mt_path_free(path);
+    // Add leaves to tree
+    for (int i = 0; i < leaves_len; i++){
+        tree.append(leaves[i]);
     }
 
-    //Free the tree
-    ginger_mt_free(tree);
+    // Finalize tree
+    tree.finalize_in_place();
 
-    //Free the root
-    zendoo_field_free(root);
+    // Compute root and assert equality with expected one
+    auto root = tree.root();
+    ASSERT_TRUE(("Expected roots to be equal", zendoo_field_assert_eq(root, expected_root)));
 
-    //Free all the leaves
+    // It is the same by calling finalize()
+    auto tree_copy = tree.finalize();
+    auto root_copy = tree_copy.root();
+    ASSERT_TRUE(("Expected roots to be equal", zendoo_field_assert_eq(root_copy, expected_root)));
+
+    // Free memory
+    zendoo_field_free(expected_root);
     for (int i = 0; i < leaves_len; i++){
         zendoo_field_free((field_t*)leaves[i]);
     }
+    zendoo_field_free(root);
+    zendoo_field_free(root_copy);
 }
 
 // Execute the test from zen directory
@@ -126,20 +144,20 @@ TEST(ZendooLib, TestProof)
 
     //Inputs
     unsigned char end_epoch_mc_b_hash[32] = {
-        78, 85, 161, 67, 167, 192, 185, 56, 133, 49, 134, 253, 133, 165, 182, 80, 152, 93, 203, 77, 165, 13, 67, 0, 64,
-        200, 185, 46, 93, 135, 238, 70
+        157, 219, 85, 159, 75, 56, 146, 21, 107, 239, 76, 31, 208, 213, 230, 24, 44, 74, 250, 66, 71, 23, 106, 4, 138,
+        157, 28, 43, 158, 39, 152, 91
     };
 
     unsigned char prev_end_epoch_mc_b_hash[32] = {
-        68, 214, 34, 70, 20, 109, 48, 39, 210, 156, 109, 60, 139, 15, 102, 79, 79, 2, 87, 190, 118, 38, 54, 18, 170, 67,
-        212, 205, 183, 115, 182, 198
+        74, 229, 219, 59, 25, 231, 227, 68, 3, 118, 194, 58, 99, 219, 112, 39, 73, 202, 238, 140, 114, 144, 253, 32,
+        237, 117, 117, 60, 200, 70, 187, 171
     };
 
     unsigned char constant_bytes[96] = {
-        170, 190, 140, 27, 234, 135, 240, 226, 158, 16, 29, 161, 178, 36, 69, 34, 29, 75, 195, 247, 29, 93, 92, 48, 214,
-        102, 70, 134, 68, 165, 170, 201, 119, 162, 19, 254, 229, 115, 80, 248, 106, 182, 164, 40, 21, 154, 15, 177, 158,
-        16, 172, 169, 189, 253, 206, 182, 72, 183, 128, 160, 182, 39, 98, 76, 95, 198, 62, 39, 87, 213, 251, 12, 154,
-        180, 125, 231, 222, 73, 129, 120, 144, 197, 116, 248, 95, 206, 147, 108, 252, 125, 79, 118, 57, 26, 0, 0
+        234, 144, 148, 15, 127, 44, 243, 131, 152, 238, 209, 246, 126, 175, 154, 42, 208, 215, 180, 233, 20, 153, 7, 10,
+        180, 78, 89, 9, 9, 160, 1, 42, 91, 202, 221, 104, 241, 231, 8, 59, 174, 159, 27, 108, 74, 80, 118, 192, 127, 238,
+        216, 167, 72, 15, 61, 97, 121, 13, 48, 143, 255, 165, 228, 6, 121, 210, 112, 228, 161, 214, 233, 137, 108, 184,
+        80, 27, 213, 72, 110, 7, 200, 194, 23, 95, 102, 236, 181, 230, 139, 215, 104, 22, 214, 70, 0, 0
     };
 
     auto constant = zendoo_deserialize_field(constant_bytes);
@@ -199,20 +217,20 @@ TEST(ZendooLib, TestProofNoBwt){
 
     //Inputs
     unsigned char end_epoch_mc_b_hash[32] = {
-        28, 207, 62, 204, 135, 33, 168, 143, 231, 177, 64, 181, 184, 237, 93, 185, 196, 115, 241, 65, 176, 205, 254, 83,
-        216, 229, 119, 73, 184, 217, 26, 109
+        8, 57, 79, 205, 58, 30, 190, 170, 144, 137, 231, 236, 172, 54, 173, 50, 69, 208, 163, 134, 201, 131, 129, 223,
+        143, 76, 119, 48, 95, 6, 141, 17
     };
 
     unsigned char prev_end_epoch_mc_b_hash[32] = {
-        64, 236, 160, 62, 217, 6, 240, 243, 184, 32, 158, 223, 218, 177, 165, 121, 12, 124, 153, 137, 218, 208, 152, 125,
-        187, 145, 172, 244, 223, 220, 234, 195
+        172, 64, 135, 162, 30, 208, 207, 7, 107, 205, 4, 141, 230, 6, 119, 131, 112, 98, 170, 234, 70, 66, 95, 11, 159,
+        178, 50, 37, 95, 187, 147, 1
     };
 
     unsigned char constant_bytes[96] = {
-        249, 199, 228, 179, 227, 163, 140, 243, 174, 240, 187, 245, 152, 245, 74, 136, 36, 142, 231, 196, 162, 148, 139,
-        157, 198, 117, 186, 83, 72, 103, 121, 253, 5, 64, 230, 173, 84, 236, 12, 3, 199, 26, 171, 58, 141, 171, 85, 151,
-        209, 228, 76, 0, 21, 241, 65, 100, 50, 194, 8, 163, 121, 129, 242, 124, 166, 105, 158, 76, 146, 169, 188, 243,
-        188, 82, 176, 244, 255, 122, 125, 90, 154, 45, 12, 223, 62, 156, 140, 20, 35, 83, 55, 111, 47, 10, 1, 0
+        53, 15, 18, 36, 121, 179, 90, 14, 215, 218, 231, 181, 9, 186, 122, 78, 227, 142, 190, 43, 134, 218, 178, 160,
+        251, 246, 207, 130, 247, 53, 246, 68, 251, 126, 22, 250, 0, 135, 243, 13, 97, 76, 166, 142, 143, 19, 69, 66,
+        225, 142, 210, 176, 253, 197, 145, 68, 142, 4, 96, 91, 23, 39, 56, 43, 96, 115, 57, 59, 34, 62, 156, 221, 27,
+        174, 134, 170, 26, 86, 112, 176, 126, 207, 29, 213, 99, 3, 183, 43, 191, 43, 211, 110, 177, 152, 0, 0
     };
 
     auto constant = zendoo_deserialize_field(constant_bytes);
