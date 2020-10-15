fb9e49f38f86cd1e61c047e75689e6b5dc24b2f3
cronicc <cronic@zensystem.io>
2020-10-15 06:54:01 +0000

    Rename threads from zcash to horizen

--- src/init.cpp
+++ src/init.cpp
@@ -188,7 +188,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("zcash-shutoff");
+    RenameThread("horizen-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
@@ -633,7 +633,7 @@ void CleanupBlockRevFiles()
 
 void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 {
-    RenameThread("zcash-loadblk");
+    RenameThread("horizen-loadblk");
     // -reindex
     if (fReindex) {
         CImportingNow imp;
