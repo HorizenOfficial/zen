e5fc3f0bc8bd5e509ac1a9460bc052a0d56e073e
abi87 <alberto.benegiamo@gmail.com>
2020-11-17 12:22:06 +0100

    Added UT file for reindex

---
+++ src/gtest/test_reindex.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+
+//includes for helpers
+#include <boost/filesystem.hpp>
+#include <util.h>
+
+//includes for sut
+#include <main.h>
+
+class ReindexTestSuite: public ::testing::Test {
+public:
+    ReindexTestSuite():
+        dataDirLocation(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path())
+    {
+        SelectParams(CBaseChainParams::REGTEST);
+        boost::filesystem::create_directories(dataDirLocation);
+        mapArgs["-datadir"] = dataDirLocation.string();
+    };
+    ~ReindexTestSuite()
+    {
+        ClearDatadirCache();
+        boost::system::error_code ec;
+        boost::filesystem::remove_all(dataDirLocation.string(), ec);
+    };
+
+    void SetUp() override {};
+    void TearDown() override {};
+
+private:
+    boost::filesystem::path  dataDirLocation;
+};
+
+TEST_F(ReindexTestSuite, EmptyTest1) {
+    EXPECT_TRUE(true);
+}
+
+TEST_F(ReindexTestSuite, EmptyTest2) {
+    EXPECT_FALSE(false);
+}
+
+TEST_F(ReindexTestSuite, LoadingBlocksFromNullFilePtrWillAbort) {
+	//prerequisites
+    int blkFileSuffix = {1987};
+
+    CDiskBlockPos diskBlkPos(blkFileSuffix, 0);
+    ASSERT_TRUE(!boost::filesystem::exists(GetBlockPosFilename(diskBlkPos, "blk")));
+
+    //test
+    FILE* filePtr = OpenBlockFile(diskBlkPos, /*fReadOnly*/true);
+
+    //checks
+    EXPECT_TRUE(filePtr == nullptr);
+    EXPECT_EXIT(LoadBlocksFromExternalFile(filePtr, &diskBlkPos), ::testing::KilledBySignal(SIGSEGV),".*");
+}
+
+TEST_F(ReindexTestSuite, BlocksAreNotLoadedFromEmptyBlkFile) {
+	//prerequisites
+	int blkFileSuffix = {1};
+
+    CDiskBlockPos diskBlkPos(blkFileSuffix, 0);
+    ASSERT_TRUE(!boost::filesystem::exists(GetBlockPosFilename(diskBlkPos, "blk")));
+
+    //opening not read-only creates the file
+    FILE* filePtr = OpenBlockFile(diskBlkPos, /*fReadOnly*/false);
+    ASSERT_TRUE(filePtr != nullptr);
+
+    //test
+    bool res = LoadBlocksFromExternalFile(filePtr, &diskBlkPos);
+
+    //checks
+    EXPECT_FALSE(res);
+}
