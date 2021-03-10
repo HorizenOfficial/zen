fb832367b56a558e563ced97ddaf200dacee8f62
abi87 <alberto.benegiamo@gmail.com>
2021-03-10 10:30:48 +0100

    Minor UT refactoring

--- src/gtest/test_libzendoo.cpp
+++ src/gtest/test_libzendoo.cpp
@@ -153,6 +153,40 @@ TEST(SidechainsField, CopyAndAssignement)
     }
 }
 
+TEST(SidechainsField, PoseidonHashTest)
+{
+    std::vector<unsigned char> lhs {
+        138, 206, 199, 243, 195, 254, 25, 94, 236, 155, 232, 182, 89, 123, 162, 207, 102, 52, 178, 128, 55, 248, 234,
+        95, 33, 196, 170, 12, 118, 16, 124, 96, 47, 203, 160, 167, 144, 153, 161, 86, 213, 126, 95, 76, 27, 98, 34, 111,
+        144, 36, 205, 124, 200, 168, 29, 196, 67, 210, 100, 154, 38, 79, 178, 191, 246, 115, 84, 232, 87, 12, 34, 72,
+        88, 23, 236, 142, 237, 45, 11, 148, 91, 112, 156, 47, 68, 229, 216, 56, 238, 98, 41, 243, 225, 192, 0, 0
+    };
+    CSidechainField lhsField{lhs};
+    ASSERT_TRUE(lhsField.IsValid());
+
+    std::vector<unsigned char> rhs {
+        199, 130, 235, 52, 44, 219, 5, 195, 71, 154, 54, 121, 3, 11, 111, 160, 86, 212, 189, 66, 235, 236, 240, 242,
+        126, 248, 116, 0, 48, 95, 133, 85, 73, 150, 110, 169, 16, 88, 136, 34, 106, 7, 38, 176, 46, 89, 163, 49, 162,
+        222, 182, 42, 200, 240, 149, 226, 173, 203, 148, 194, 207, 59, 44, 185, 67, 134, 107, 221, 188, 208, 122, 212,
+        200, 42, 227, 3, 23, 59, 31, 37, 91, 64, 69, 196, 74, 195, 24, 5, 165, 25, 101, 215, 45, 92, 1, 0
+    };
+    CSidechainField rhsField{rhs};
+    ASSERT_TRUE(rhsField.IsValid());
+
+    std::vector<unsigned char> expectedHash {
+        53, 2, 235, 12, 255, 18, 125, 167, 223, 32, 245, 103, 38, 74, 43, 73, 254, 189, 174, 137, 20, 90, 195, 107, 202,
+        24, 151, 136, 85, 23, 9, 93, 207, 33, 229, 200, 178, 225, 221, 127, 18, 250, 108, 56, 86, 94, 171, 1, 76, 21,
+        237, 254, 26, 235, 196, 14, 18, 129, 101, 158, 136, 103, 147, 147, 239, 140, 163, 94, 245, 147, 110, 28, 93,
+        231, 66, 7, 111, 11, 202, 99, 146, 211, 117, 143, 224, 99, 183, 108, 157, 200, 119, 169, 180, 148, 0, 0,
+    };
+    CSidechainField expectedField{expectedHash};
+    ASSERT_TRUE(expectedField.IsValid());
+
+    EXPECT_TRUE(CSidechainField::ComputeHash(lhsField, rhsField) == expectedField)
+    <<"expectedField "<<expectedField.GetHexRepr()<<"\n"
+	<<"actualField   "<<CSidechainField::ComputeHash(lhsField, rhsField).GetHexRepr();
+}
+
 TEST(ZendooLib, FieldTest)
 {
     //Size is the expected one
@@ -172,52 +206,6 @@ TEST(ZendooLib, FieldTest)
 
     zendoo_field_free(field);
     zendoo_field_free(field_deserialized);
-}
-
-
-TEST(ZendooLib, PoseidonHashTest) 
-{
-    unsigned char lhs[96] = {
-        138, 206, 199, 243, 195, 254, 25, 94, 236, 155, 232, 182, 89, 123, 162, 207, 102, 52, 178, 128, 55, 248, 234,
-        95, 33, 196, 170, 12, 118, 16, 124, 96, 47, 203, 160, 167, 144, 153, 161, 86, 213, 126, 95, 76, 27, 98, 34, 111,
-        144, 36, 205, 124, 200, 168, 29, 196, 67, 210, 100, 154, 38, 79, 178, 191, 246, 115, 84, 232, 87, 12, 34, 72,
-        88, 23, 236, 142, 237, 45, 11, 148, 91, 112, 156, 47, 68, 229, 216, 56, 238, 98, 41, 243, 225, 192, 0, 0
-    };
-
-    unsigned char rhs[96] = {
-        199, 130, 235, 52, 44, 219, 5, 195, 71, 154, 54, 121, 3, 11, 111, 160, 86, 212, 189, 66, 235, 236, 240, 242,
-        126, 248, 116, 0, 48, 95, 133, 85, 73, 150, 110, 169, 16, 88, 136, 34, 106, 7, 38, 176, 46, 89, 163, 49, 162,
-        222, 182, 42, 200, 240, 149, 226, 173, 203, 148, 194, 207, 59, 44, 185, 67, 134, 107, 221, 188, 208, 122, 212,
-        200, 42, 227, 3, 23, 59, 31, 37, 91, 64, 69, 196, 74, 195, 24, 5, 165, 25, 101, 215, 45, 92, 1, 0
-    };
-
-    unsigned char hash[96] = {
-        53, 2, 235, 12, 255, 18, 125, 167, 223, 32, 245, 103, 38, 74, 43, 73, 254, 189, 174, 137, 20, 90, 195, 107, 202,
-        24, 151, 136, 85, 23, 9, 93, 207, 33, 229, 200, 178, 225, 221, 127, 18, 250, 108, 56, 86, 94, 171, 1, 76, 21,
-        237, 254, 26, 235, 196, 14, 18, 129, 101, 158, 136, 103, 147, 147, 239, 140, 163, 94, 245, 147, 110, 28, 93,
-        231, 66, 7, 111, 11, 202, 99, 146, 211, 117, 143, 224, 99, 183, 108, 157, 200, 119, 169, 180, 148, 0, 0,
-    };
-
-    auto lhs_field = zendoo_deserialize_field(lhs);
-    ASSERT_TRUE(lhs_field != NULL);
-
-    auto rhs_field = zendoo_deserialize_field(rhs);
-    ASSERT_TRUE(rhs_field != NULL);
-
-    auto expected_hash = zendoo_deserialize_field(hash);
-    ASSERT_TRUE(expected_hash != NULL);
-
-    const field_t* hash_input[] = {lhs_field, rhs_field};
-
-    auto actual_hash = zendoo_compute_poseidon_hash(hash_input, 2);
-    ASSERT_TRUE(actual_hash != NULL);
-
-    ASSERT_TRUE(zendoo_field_assert_eq(expected_hash, actual_hash));
-
-    zendoo_field_free(lhs_field);
-    zendoo_field_free(rhs_field);
-    zendoo_field_free(expected_hash);
-    zendoo_field_free(actual_hash);
 }
 
 TEST(ZendooLib, PoseidonMerkleTreeTest)  {
