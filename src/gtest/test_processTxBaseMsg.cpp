4edd90875705ff1d7c4edc63b2d62763a8766167
abi87 <alberto.benegiamo@gmail.com>
2021-03-25 18:55:30 +0100

    Added UTs for ProcessTxBaseMsg

--- src/gtest/test_processTxBaseMsg.cpp
+++ src/gtest/test_processTxBaseMsg.cpp
@@ -5,6 +5,7 @@
 #include <protocol.h>
 #include <txdb.h>
 #include <undo.h>
+#include <consensus/validation.h>
 
 class CCoinsOnlyViewDB : public CCoinsViewDB
 {
@@ -27,7 +28,7 @@ public:
 };
 
 MempoolReturnValue ValidTxInMempool(CTxMemPool& pool, CValidationState &state, const CTransactionBase &txBase,
-                                 LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
+                                    LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
     {
         if (txBase.IsCertificate())
         {
@@ -43,18 +44,29 @@ MempoolReturnValue ValidTxInMempool(CTxMemPool& pool, CValidationState &state, c
     }
 
 MempoolReturnValue OrphanTxInMempool(CTxMemPool& pool, CValidationState &state, const CTransactionBase &txBase,
-                                 LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
+                                     LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
     {return MempoolReturnValue::MISSING_INPUT;}
 
-MempoolReturnValue InvalidTxInMempool(CTxMemPool& pool, CValidationState &state, const CTransactionBase &txBase,
-                                 LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
-    {return MempoolReturnValue::INVALID;}
+MempoolReturnValue InvalidZeroDosTxInMempool(CTxMemPool& pool, CValidationState &state, const CTransactionBase &txBase,
+                                      LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
+    {
+        state.DoS(0, false, REJECT_INVALID);
+        return MempoolReturnValue::INVALID;
+    }
+
+MempoolReturnValue InvalidHighDosTxInMempool(CTxMemPool& pool, CValidationState &state, const CTransactionBase &txBase,
+                                      LimitFreeFlag fLimitFree, RejectAbsurdFeeFlag fRejectAbsurdFee)
+    {
+        state.DoS(100, false, REJECT_INVALID);
+        return MempoolReturnValue::INVALID;
+    }
 
 class ProcessTxBaseMsgTestSuite : public ::testing::Test
 {
 public:
     ProcessTxBaseMsgTestSuite():
-        ValidTxMock(::ValidTxInMempool), OrphanTxMock(::OrphanTxInMempool), InvalidTxMock(::InvalidTxInMempool),
+        ValidTxMock(::ValidTxInMempool), OrphanTxMock(::OrphanTxInMempool),
+        InvalidZeroDosTxMock(::InvalidZeroDosTxInMempool), InvalidHighDosTxMock(::InvalidHighDosTxInMempool),
         pathTemp(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path()),
         chainStateDbSize(0), pChainStateDb(nullptr) {};
 
@@ -75,6 +87,7 @@ public:
 
     void TearDown() override {
         UnloadBlockIndex();
+        mapRelay.clear();
 
         delete pChainStateDb;
         pChainStateDb = nullptr;
@@ -85,7 +98,8 @@ public:
 protected:
     processMempoolTx ValidTxMock;
     processMempoolTx OrphanTxMock;
-    processMempoolTx InvalidTxMock;
+    processMempoolTx InvalidZeroDosTxMock;
+    processMempoolTx InvalidHighDosTxMock;
 
 private:
     boost::filesystem::path  pathTemp;
@@ -95,7 +109,6 @@ private:
 
 TEST_F(ProcessTxBaseMsgTestSuite, ValidUnknownTxIsRelayed)
 {
-    //prepare an unknown, valid tx
     CTransaction aKnownTx = txCreationUtils::createTransparentTx();
     ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
 
@@ -113,7 +126,6 @@ TEST_F(ProcessTxBaseMsgTestSuite, ValidUnknownTxIsRelayed)
 
 TEST_F(ProcessTxBaseMsgTestSuite, ValidUnknownTxIsRecordedAsKnown)
 {
-    //prepare an unknown, valid tx
     CTransaction aKnownTx = txCreationUtils::createTransparentTx();
     ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
 
@@ -124,6 +136,126 @@ TEST_F(ProcessTxBaseMsgTestSuite, ValidUnknownTxIsRecordedAsKnown)
 
     //test
     ProcessTxBaseMsg(aKnownTx, &sourceNode, ValidTxMock);
+
+    //checks
+    EXPECT_TRUE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, MissingInputsNonJoinsplitUnknownTxIsNotRelayed)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, OrphanTxMock);
+
+    //checks
+    EXPECT_TRUE(mapRelay.count(CInv(MSG_TX, aKnownTx.GetHash())) == 0);
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, MissingInputsNonJoinsplitUnknownTxIsRecordedAsKnown)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, OrphanTxMock);
+
+    //checks
+    EXPECT_TRUE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, InvalidZeroDosUnknownTxIsNotRelayed)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, InvalidZeroDosTxMock);
+
+    //checks
+    EXPECT_TRUE(mapRelay.count(CInv(MSG_TX, aKnownTx.GetHash())) == 0);
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, InvalidZeroDosUnknownTxFromWhitelistedPeerIsRelayed)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+    sourceNode.fWhitelisted = true;
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, InvalidZeroDosTxMock);
+
+    //checks
+    EXPECT_TRUE(mapRelay.count(CInv(MSG_TX, aKnownTx.GetHash())) != 0);
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, InvalidZeroDosUnknownUnknownTxIsRecordedAsKnown)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, InvalidZeroDosTxMock);
+
+    //checks
+    EXPECT_TRUE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, InvalidHighDosUnknownTxIsNotRelayed)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, InvalidHighDosTxMock);
+
+    //checks
+    EXPECT_TRUE(mapRelay.count(CInv(MSG_TX, aKnownTx.GetHash())) == 0);
+}
+
+TEST_F(ProcessTxBaseMsgTestSuite, InvalidHighDosUnknownUnknownTxIsRecordedAsKnown)
+{
+    CTransaction aKnownTx = txCreationUtils::createTransparentTx();
+    ASSERT_FALSE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
+
+    //Prepare node from which tx originates
+    CNode::ClearBanned();
+    CAddress dummyAddr(CService(CNetAddr{}, Params().GetDefaultPort()));
+    CNode sourceNode(INVALID_SOCKET, dummyAddr, "", true);
+
+    //test
+    ProcessTxBaseMsg(aKnownTx, &sourceNode, InvalidHighDosTxMock);
 
     //checks
     EXPECT_TRUE(AlreadyHave(CInv(MSG_TX, aKnownTx.GetHash())));
