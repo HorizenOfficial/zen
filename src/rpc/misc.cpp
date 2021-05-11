2e397fd93f037cae1cb3fc46324dd122f0ddf2ec
luisantoniocrag <42625753+luisantoniocrag@users.noreply.github.com>
2021-05-11 07:12:00 -0500

    Update src/rpc/misc.cpp
    Co-authored-by: cronicc <cronic@zensystem.io>

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -221,7 +221,7 @@ UniValue z_validateaddress(const UniValue& params, bool fHelp)
     if (fHelp || params.size() != 1)
         throw runtime_error(
             "z_validateaddress \"zaddr\"\n"
-            "\nVerify an zaddress is valid.\n"
+            "\nReturn information about the given zaddress.\n"
 
             "\nArguments:\n"
             "1. \"zaddr\"                       (string, required) the zaddress to validate\n"
