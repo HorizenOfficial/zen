499ec835ff6ab398659dc4449b2f6a8d476e5086
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2021-06-03 18:11:17 +0200

    Added a Python test for CSW management in mempool.
    Added a new Python test to check that CSW transactions in mempool are rejected in case the sidechain passes from "ceased" to "alive" (due to a chain fork and the disconnection of the ceasing block).
    
    Removed some unused code and variables from sc_cert_memcleanup_split.py.

--- qa/rpc-tests/sc_csw_memcleanup_split.py
+++ qa/rpc-tests/sc_csw_memcleanup_split.py
@@ -4,7 +4,9 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #
-# Test checkcswnullifier for presence nullifer in MC.
+# Test that a CSW transaction gets removed from mempool in case a sidechain
+# passes from state "CEASED" to "ALIVE" (due to a blockchain fork and the
+# revert of some blocks).
 #
 
 from test_framework.test_framework import BitcoinTestFramework
@@ -12,7 +14,7 @@ from test_framework.authproxy import JSONRPCException
 from test_framework.util import assert_equal, initialize_chain_clean, \
     start_nodes, connect_nodes_bi, assert_true, assert_false, mark_logs, \
     wait_bitcoinds, stop_nodes, get_epoch_data, sync_mempools, sync_blocks, \
-    disconnect_nodes, advance_epoch
+    disconnect_nodes, advance_epoch, swap_bytes
 
 from test_framework.mc_test.mc_test import CertTestUtils, CSWTestUtils, generate_random_field_element_hex
 
@@ -67,10 +69,10 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
     def run_test(self):
         '''
         Create a SC, advance two epochs, move to the limit of the safe guard and then split the network. 
-        One network part sends fwt, mbtr and a certificate, all of them are stored in mempool.
-        The other network part sends a certificate and mines one block reaching a longer chain height.
-        When the network is joined, verify that the SC is alive, fwt is still in the mempool of the the
-        loosing network part, but cert and mbtr have been removed from those mempool.
+        One network part sends a certificate to keep the sidechain alive and then generates two blocks.
+        The other network part generates one block, theny sends a CSW.
+        When the network is joined, verify that the SC is alive and that the CSW transaction has been
+        removed from mempool.
         '''
 
         # prepare some coins 
@@ -92,9 +94,11 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
         sc_cr_amount = Decimal('12.00000000')
 
         certMcTest = CertTestUtils(self.options.tmpdir, self.options.srcdir)
+        cswMcTest = CSWTestUtils(self.options.tmpdir, self.options.srcdir)
 
         # generate wCertVk and constant
         vk = certMcTest.generate_params("sc1")
+        cswVk = cswMcTest.generate_params("csw1")
         constant = generate_random_field_element_hex()
 
         cmdInput = {
@@ -102,6 +106,7 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
             'amount': sc_cr_amount,
             'toaddress': sc_address,
             'wCertVk': vk,
+            'wCeasedVk': cswVk,
             'constant': constant,
             'mainchainBackwardTransferRequestDataLength': 1
         }
@@ -111,9 +116,6 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
         scid = res['scid']
         self.sync_all()
         mark_logs("tx {} created SC {}".format(tx, scid), self.nodes, DEBUG_MODE)
-
-        pkh1 = self.nodes[1].getnewaddress("", True)
-        fe1 = [generate_random_field_element_hex()]
 
         # advance two epochs
         mark_logs("\nLet 2 epochs pass by...", self.nodes, DEBUG_MODE)
@@ -139,8 +141,6 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
         self.sync_all()
         print "Node0 Chain h = ", self.nodes[0].getblockcount()
         
-        bal_initial = self.nodes[0].getscinfo(scid, False, False)['items'][0]['balance']
-
         #============================================================================================
         mark_logs("\nSplit network", self.nodes, DEBUG_MODE)
         self.split_network()
@@ -148,29 +148,6 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
 
         # Network part 0-1-2
         print "------------------"
-        # use different nodes for sending txes and cert in order to be sure there are no dependancies from each other
-        fwt_amount = Decimal("2.0")
-        mark_logs("\nNTW part 1) Node0 sends {} coins to SC".format(fwt_amount), self.nodes, DEBUG_MODE)
-        tx_fwd = self.nodes[0].sc_send("abcd", fwt_amount, scid)
-        sync_mempools(self.nodes[0:3])
-
-        mark_logs("              Check fwd tx {} is in mempool".format(tx_fwd), self.nodes, DEBUG_MODE)
-        assert_true(tx_fwd in self.nodes[0].getrawmempool()) 
-
-        outputs = [{'vScRequestData':fe1, 'scFee':Decimal("0.001"), 'scid':scid, 'pubkeyhash':pkh1 }]
-        cmdParms = { "minconf":0, "fee":0.0}
-        mark_logs("\nNTW part 1) Node1 creates a tx with a bwt request", self.nodes, DEBUG_MODE)
-        try:
-            tx_bwt = self.nodes[1].request_transfer_from_sidechain(outputs, cmdParms)
-        except JSONRPCException, e:
-            errorString = e.error['message']
-            mark_logs(errorString,self.nodes,DEBUG_MODE)
-            assert_true(False)
-
-        sync_mempools(self.nodes[0:3])
-
-        mark_logs("              Check bwd tx {} is in mempool".format(tx_bwt), self.nodes, DEBUG_MODE)
-        assert_true(tx_bwt in self.nodes[0].getrawmempool()) 
 
         mark_logs("\nNTW part 1) Node2 sends a certificate", self.nodes, DEBUG_MODE)
         epoch_number, epoch_cum_tree_hash = get_epoch_data(scid, self.nodes[2], sc_epoch_len)
@@ -191,99 +168,87 @@ class CertMempoolCleanupSplit(BitcoinTestFramework):
             assert(False)
         sync_mempools(self.nodes[0:3])
 
-        mark_logs("              Check cert {} is in mempool".format(cert_bad), self.nodes, DEBUG_MODE)
-        assert_true(cert_bad in self.nodes[0].getrawmempool()) 
+        mark_logs("Check cert {} is in mempool".format(cert_bad), self.nodes, DEBUG_MODE)
+        assert_true(cert_bad in self.nodes[0].getrawmempool())
+
+        mark_logs("Generates two blocks to make the chain longer than sub-network 2", self.nodes, DEBUG_MODE)
+        self.nodes[0].generate(2)
+
         print "Node0 Chain h = ", self.nodes[0].getblockcount()
 
         # Network part 2
         #------------------
-        mark_logs("\nNTW part 2) Node3 sends a certificate", self.nodes, DEBUG_MODE)
-        epoch_number, epoch_cum_tree_hash = get_epoch_data(scid, self.nodes[3], sc_epoch_len)
 
-        bt_amount_2 = Decimal("10.0")
-        pkh_node1 = self.nodes[1].getnewaddress("", True)
-        quality = 5
+        mark_logs("\nNTW part 2) Node3 generates one block to cease the sidechain", self.nodes, DEBUG_MODE)
+        self.nodes[3].generate(1)
 
-        proof = certMcTest.create_test_proof("sc1", epoch_number, quality, MBTR_SC_FEE, FT_SC_FEE, constant, epoch_cum_tree_hash, [pkh_node1], [bt_amount_2])
+        mark_logs("Check that the sidechain is ceased from node 3 perspective", self.nodes, DEBUG_MODE)
+        ret = self.nodes[3].getscinfo(scid, False, False)['items'][0]
+        assert_equal(ret['state'], "CEASED")
 
-        amount_cert = [{"pubkeyhash": pkh_node1, "amount": bt_amount_2}]
+        sc_bal = ret['balance']
+
+        mark_logs("Node 3 creates a CSW transaction", self.nodes, DEBUG_MODE)
+
+        csw_mc_address = self.nodes[3].getnewaddress()
+        taddr = self.nodes[3].getnewaddress()
+        sc_csw_amount = sc_bal
+        null = generate_random_field_element_hex()
+        actCertData            = self.nodes[3].getactivecertdatahash(scid)['certDataHash']
+        pkh_mc_address         = self.nodes[3].validateaddress(csw_mc_address)['pubkeyhash']
+        ceasingCumScTxCommTree = self.nodes[3].getceasingcumsccommtreehash(scid)['ceasingCumScTxCommTree']
+
+        scid_swapped = swap_bytes(scid)
+        csw_proof = cswMcTest.create_test_proof(
+                "csw1", sc_csw_amount, str(scid_swapped), null, pkh_mc_address, ceasingCumScTxCommTree, actCertData)
+
+        sc_csws = [{
+            "amount": sc_csw_amount,
+            "senderAddress": csw_mc_address,
+            "scId": scid,
+            "epoch": 0,
+            "nullifier": null,
+            "activeCertData": actCertData,
+            "ceasingCumScTxCommTree": ceasingCumScTxCommTree,
+            "scProof": csw_proof
+        }]
+
+        out_amount = sc_csw_amount / Decimal("2.0")
+        sc_csw_tx_outs = {taddr: out_amount}
+        rawtx = self.nodes[3].createrawtransaction([], sc_csw_tx_outs, sc_csws)
+        funded_tx = self.nodes[3].fundrawtransaction(rawtx)
+        sigRawtx = self.nodes[3].signrawtransaction(funded_tx['hex'])
         try:
-            cert = self.nodes[3].send_certificate(scid, epoch_number, quality,
-                epoch_cum_tree_hash, proof, amount_cert, 0, 0, 0.01)
+            csw_bad = self.nodes[3].sendrawtransaction(sigRawtx['hex'])
+            pprint.pprint(self.nodes[3].getrawtransaction(tx, 1))
+            assert(False)
         except JSONRPCException, e:
             errorString = e.error['message']
-            print "Send certificate failed with reason {}".format(errorString)
-            assert(False)
-        sync_mempools(self.nodes[3:4])
+            mark_logs("Send csw failed with reason {}".format(errorString), self.nodes, DEBUG_MODE)
 
-        mark_logs("              Check cert {} is in mempool".format(cert), self.nodes, DEBUG_MODE)
-        assert_true(cert in self.nodes[3].getrawmempool())
-        print "Node3 Chain h = ", self.nodes[3].getblockcount()
-
-
-        mark_logs("Node3 generates 1 block", self.nodes, DEBUG_MODE)
-        self.nodes[3].generate(1)
-        sync_mempools(self.nodes[3:4])
-        print "Node3 Chain h = ", self.nodes[3].getblockcount()
+        mark_logs("Check the CSW tx {} is in mempool...".format(csw_bad), self.nodes, DEBUG_MODE)
+        assert_true(csw_bad in self.nodes[3].getrawmempool())
 
         #============================================================================================
         mark_logs("\nJoining network", self.nodes, DEBUG_MODE)
         self.join_network()
         mark_logs("Network joined", self.nodes, DEBUG_MODE)
 
-        # check SC is alive
+        mark_logs("\nCheck that the sidechain is ceased", self.nodes, DEBUG_MODE)
         ret = self.nodes[3].getscinfo(scid, False, False)['items'][0]
         assert_equal(ret['state'], "ALIVE")
 
-        mark_logs("Check fwd tx {} is still in mempool...".format(tx_fwd), self.nodes, DEBUG_MODE)
-        assert_true(tx_fwd in self.nodes[0].getrawmempool()) 
-
-        any_error = False
-
-        mark_logs("Check bwd tx {} is no more in mempool, since we crossed the epoch safeguard".format(tx_bwt), self.nodes, DEBUG_MODE)
-        #assert_false(tx_bwt in self.nodes[0].getrawmempool()) 
-        if tx_bwt in self.nodes[0].getrawmempool():
-            print "FIX FIX FIX!!! bwt is still in mempool" 
-            any_error = True
+        mark_logs("Check CSW tx {} is no more in mempool, since we crossed the epoch safeguard".format(csw_bad), self.nodes, DEBUG_MODE)
+        assert_false(csw_bad in self.nodes[3].getrawmempool()) 
 
         mark_logs("And that no info are available too...", self.nodes, DEBUG_MODE)
         try:
-            self.nodes[0].getrawtransaction(tx_bwt, 1)
-            print "FIX FIX FIX!!! tx_bwt has info in Node0" 
-            any_error = True
-            #assert (False)
+            self.nodes[3].getrawtransaction(csw_bad, 1)
+            assert(False)
         except JSONRPCException, e:
             errorString = e.error['message']
             mark_logs("===> {}".format(errorString), self.nodes, DEBUG_MODE)
             assert_true("No information" in errorString)
-
-        mark_logs("Check cert {} is no more in mempool, since we crossed the epoch safeguard".format(cert_bad), self.nodes, DEBUG_MODE)
-        assert_false(cert_bad in self.nodes[0].getrawmempool()) 
-        if cert_bad in self.nodes[0].getrawmempool():
-            print "FIX FIX FIX!!! cert is still in mempool" 
-            any_error = True
-
-        mark_logs("And that no info are available too...", self.nodes, DEBUG_MODE)
-        try:
-            self.nodes[0].getrawcertificate(cert_bad, 1)
-            print "FIX FIX FIX!!! cert has info in Node0" 
-            any_error = True
-            assert (False)
-        except JSONRPCException, e:
-            errorString = e.error['message']
-            mark_logs("===> {}".format(errorString), self.nodes, DEBUG_MODE)
-
-        mark_logs("Check SC balance has been properly changed", self.nodes, DEBUG_MODE)
-        try:
-            bal_final = self.nodes[0].getscinfo(scid)['items'][0]['balance']
-            assert_equal(bal_initial - bt_amount_2, bal_final)
-        except JSONRPCException, e:
-            errorString = e.error['message']
-            mark_logs("===> {}".format(errorString), self.nodes, DEBUG_MODE)
-
-        if any_error:
-            print" =========================> Test failed!!!"
-            #assert(False)
 
         for i in range(NUMB_OF_NODES):
             pprint.pprint(self.nodes[i].getrawmempool())
