2408141a16264534c59a69c2ea216ad37ef93a53
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-06-04 13:05:40 -0400

    WIP: Sync with mc-cryptolib: failing proofs ids + batch verification with priority + segment size and num constraints to pass to test functions

--- depends/packages/libzendoo.mk
+++ depends/packages/libzendoo.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1.0
 $(package)_download_path=https://github.com/HorizenOfficial/zendoo-mc-cryptolib/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=e663344968b75f6db5079d9d2fa907af992b0d0b1b958789d9e2bce7cb32d53c
-$(package)_git_commit=9c5bd816888386de5449877c347843a09fc9197d
+$(package)_sha256_hash=51c5cef10fa619d9536af68e128ade8c82bc652ea1abac86c3cd7dcb39fba0e5
+$(package)_git_commit=2177d936254ae165549b541f9cdd5ef309266b86
 $(package)_dependencies=rust $(rust_crates_z)
 $(package)_patches=cargo.config
 
