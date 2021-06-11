a8f11b4ccaf155da107ea2e66cde6d420edf7b6c
MarcoOl94 <marco.olivero94@libero.it>
2021-06-11 09:47:59 +0200

    src/zen/websocket_server.cpp

--- src/zen/websocket_server.cpp
+++ src/zen/websocket_server.cpp
@@ -36,12 +36,14 @@ namespace net = boost::asio;
 net::io_context ioc;
 
 static int MAX_BLOCKS_REQUEST = 100;
+static int MAX_HEADERS_REQUEST = 50;
 static int tot_connections = 0;
 
 class WsNotificationInterface;
 class WsHandler;
 
 static int getblock(const CBlockIndex *pindex, std::string& blockHexStr);
+static int getheader(const CBlockIndex *pindex, std::string& blockHexStr);
 static void ws_updatetip(const CBlockIndex *pindex);
 static void ws_updatemempool();
 static void ws_updatepeer();
@@ -113,10 +115,11 @@ public:
         GET_MULTIPLE_BLOCK_HASHES = 1,
 		//Request n.3 is alredy assigned to SEND_CERTIFICATE in zendoo
         GET_NEW_BLOCK_HASHES = 2,
-		GET_MEMPOOL_TXS = 4,
+        GET_MULTIPLE_BLOCK_HEADERS = 4,
         GET_RAW_MEMPOOL = 5,
 		GET_NODE_STATIC_CONFIG = 6,
 		GET_ESTIMATE_FEE = 7,
+		GET_MEMPOOL_TXS = 8,
         REQ_UNDEFINED = 0xff
     };
 
@@ -130,7 +133,7 @@ public:
 
     explicit WsEvent(WsMsgType xn): type(xn), payload(UniValue::VOBJ)
     {
-        payload.pushKV("msgType", type);
+        payload.push_back(Pair("msgType", type));
     }
     WsEvent & operator=(const WsEvent& ws) = delete;
     WsEvent(const WsEvent& ws) = delete;
@@ -159,35 +162,33 @@ private:
         WsEvent* wse = new WsEvent(WsEvent::MSG_EVENT);
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
-        rspPayload.pushKV("height", height);
-        rspPayload.pushKV("hash", strHash);
-        rspPayload.pushKV("block", blockHex);
-        rspPayload.pushKV("chainwork",  chainwork);
+        rspPayload.push_back(Pair("height", height));
+        rspPayload.push_back(Pair("hash", strHash));
+        rspPayload.push_back(Pair("block", blockHex));
+        rspPayload.push_back(Pair("chainwork",  chainwork));
 
         UniValue* rv = wse->getPayload();
-        rv->pushKV("eventType", eventType);
-        rv->pushKV("eventPayload", rspPayload);
+        rv->push_back(Pair("eventType", eventType));
+        rv->push_back(Pair("eventPayload", rspPayload));
         wsq.push(wse);
     }
 
-    void sendBlock(int height, const std::string& strHash, const std::string& blockHex, const CBlockIndex* pblockindex,
+    void sendBlock(int height, const std::string& strHash, const std::string& blockHex, const std::string& chainwork,
             WsEvent::WsMsgType msgType, const std::string clientRequestId = "")
     {
         // Send a message to the client:  type = eventType
         WsEvent* wse = new WsEvent(msgType);
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
-        rspPayload.pushKV("height", height);
-        rspPayload.pushKV("hash", strHash);
-        rspPayload.pushKV("block", blockHex);
-        if (pblockindex) {
-            rspPayload.pushKV("chainwork",  pblockindex->nChainWork.GetHex());
-        }
+        rspPayload.push_back(Pair("height", height));
+        rspPayload.push_back(Pair("hash", strHash));
+        rspPayload.push_back(Pair("block", blockHex));
+        rspPayload.push_back(Pair("chainwork", chainwork));
 
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -198,8 +199,8 @@ private:
         WsEvent* wse = new WsEvent(msgType);
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
-        rspPayload.pushKV("height", height);
-        rspPayload.pushKV("verificationprogress", Checkpoints::GuessVerificationProgress(Params().Checkpoints(), chainActive.Tip()));
+        rspPayload.push_back(Pair("height", height));
+        rspPayload.push_back(Pair("verificationprogress", Checkpoints::GuessVerificationProgress(Params().Checkpoints(), chainActive.Tip())));
 
         UniValue hashes(UniValue::VARR);
         std::list<CBlockIndex*>::iterator it = listBlock.begin();
@@ -208,12 +209,12 @@ private:
             hashes.push_back(blockIndexIterator->GetBlockHash().GetHex());
             ++it;
         }
-        rspPayload.pushKV("hashes", hashes);
+        rspPayload.push_back(Pair("hashes", hashes));
 
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -224,19 +225,19 @@ private:
         WsEvent* wse = new WsEvent(msgType);
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
-        rspPayload.pushKV("size", size);
+        rspPayload.push_back(Pair("size", size));
 
         UniValue txHashes(UniValue::VARR);
 
         for(std::list<uint256>::const_iterator it = listHashes.begin(); it != listHashes.end(); ++it) {
         	txHashes.push_back(it->GetHex());
         }
-        rspPayload.pushKV("transactions", txHashes);
+        rspPayload.push_back(Pair("transactions", txHashes));
 
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -246,18 +247,18 @@ private:
         WsEvent* wse = new WsEvent(WsEvent::MSG_EVENT);
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
-        rspPayload.pushKV("size", size);
+        rspPayload.push_back(Pair("size", size));
 
         UniValue txHashes(UniValue::VARR);
 
         for(std::list<uint256>::const_iterator it = listHashes.begin(); it != listHashes.end(); ++it) {
             txHashes.push_back(it->GetHex());
         }
-        rspPayload.pushKV("transactions", txHashes);
+        rspPayload.push_back(Pair("transactions", txHashes));
 
         UniValue* rv = wse->getPayload();
-        rv->pushKV("eventType", eventType);
-        rv->pushKV("eventPayload", rspPayload);
+        rv->push_back(Pair("eventType", eventType));
+        rv->push_back(Pair("eventPayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -273,11 +274,11 @@ private:
         for(std::list<std::string>::const_iterator it = peers.begin(); it != peers.end(); ++it) {
             peerList.push_back(it->c_str());
         }
-        rspPayload.pushKV("peers", peerList);
+        rspPayload.push_back(Pair("peers", peerList));
 
         UniValue* rv = wse->getPayload();
-        rv->pushKV("eventType", eventType);
-        rv->pushKV("eventPayload", rspPayload);
+        rv->push_back(Pair("eventType", eventType));
+        rv->push_back(Pair("eventPayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -294,11 +295,11 @@ private:
         for(std::list<std::string>::const_iterator it = listHex.begin(); it != listHex.end(); ++it) {
         	txHex.push_back(*it);
         }
-        rspPayload.pushKV("hex", txHex);
+        rspPayload.push_back(Pair("hex", txHex));
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
 
     }
@@ -312,29 +313,29 @@ private:
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
 
-        rspPayload.pushKV("version", version);
-        rspPayload.pushKV("protocolversion", protocolVersion);
-        rspPayload.pushKV("timeoffset", timeoffset);
-        rspPayload.pushKV("relayfee", relayfee);
-        rspPayload.pushKV("proxy", proxy);
-        rspPayload.pushKV("errors", errors);
-        rspPayload.pushKV("blocks", blocks);
-        rspPayload.pushKV("connections", connections);
-        rspPayload.pushKV("testnet", testnet);
-        rspPayload.pushKV("difficulty", difficulty);
-        rspPayload.pushKV("networkDifficulty", networkDifficulty);
+        rspPayload.push_back(Pair("version", version));
+        rspPayload.push_back(Pair("protocolversion", protocolVersion));
+        rspPayload.push_back(Pair("timeoffset", timeoffset));
+        rspPayload.push_back(Pair("relayfee", relayfee));
+        rspPayload.push_back(Pair("proxy", proxy));
+        rspPayload.push_back(Pair("errors", errors));
+        rspPayload.push_back(Pair("blocks", blocks));
+        rspPayload.push_back(Pair("connections", connections));
+        rspPayload.push_back(Pair("testnet", testnet));
+        rspPayload.push_back(Pair("difficulty", difficulty));
+        rspPayload.push_back(Pair("networkDifficulty", networkDifficulty));
 
         UniValue peerList(UniValue::VARR);
 
         for(std::list<std::string>::const_iterator it = peers.begin(); it != peers.end(); ++it) {
             peerList.push_back(it->c_str());
         }
-        rspPayload.pushKV("peers", peerList);
+        rspPayload.push_back(Pair("peers", peerList));
 
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -346,12 +347,28 @@ private:
         LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
         UniValue rspPayload(UniValue::VOBJ);
 
-        rspPayload.pushKV("feerate", estimatedFee);
+        rspPayload.push_back(Pair("feerate", estimatedFee));
 
         UniValue* rv = wse->getPayload();
         if (!clientRequestId.empty())
-            rv->pushKV("requestId", clientRequestId);
-        rv->pushKV("responsePayload", rspPayload);
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
+        wsq.push(wse);
+    }
+
+    void sendBlockHeaders(const UniValue& headers, WsEvent::WsMsgType msgType, std::string clientRequestId = "")
+    {
+        // Send a message to the client:  type = eventType
+        WsEvent* wse = new WsEvent(msgType);
+        LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
+        UniValue rspPayload(UniValue::VOBJ);
+
+        rspPayload.push_back(Pair("headers", headers));
+
+        UniValue* rv = wse->getPayload();
+        if (!clientRequestId.empty())
+            rv->push_back(Pair("requestId", clientRequestId));
+        rv->push_back(Pair("responsePayload", rspPayload));
         wsq.push(wse);
     }
 
@@ -409,7 +426,7 @@ private:
         {
             return ret;
         }
-        sendBlock(pblockindex->nHeight, strHash, block, pblockindex, WsEvent::MSG_RESPONSE, clientRequestId);
+        sendBlock(pblockindex->nHeight, strHash, block, pblockindex->nChainWork.GetHex(), WsEvent::MSG_RESPONSE, clientRequestId);
         return OK;
     }
 
@@ -586,6 +603,54 @@ private:
         return OK;
     }
 
+    int sendHeadersFromHashes(const UniValue& hashes, const std::string& clientRequestId)
+    {
+        if (hashes.size() > MAX_HEADERS_REQUEST) {
+            LogPrint("ws", "%s():%d - invalid hashes amount %d (max is %d)\n", __func__, __LINE__, hashes.size(), MAX_HEADERS_REQUEST);
+            return INVALID_PARAMETER;
+        }
+
+        UniValue headers(UniValue::VARR);
+
+        for (const UniValue& o : hashes.getValues()) {
+            if (o.isObject()) {
+                LogPrint("ws", "%s():%d - invalid obj\n", __func__, __LINE__);
+                return INVALID_PARAMETER;
+            }
+
+            std::string strHash = o.get_str();
+            uint256 hash(uint256S(strHash));
+            CBlockIndex* pblockindex = NULL;
+
+            {
+                LOCK(cs_main);
+
+                BlockMap::iterator mi = mapBlockIndex.find(hash);
+                if (mi != mapBlockIndex.end()) {
+                    pblockindex = (*mi).second;
+                }
+            }
+
+            if (pblockindex == NULL) {
+                LogPrint("ws", "%s():%d - block index not found for hash[%s]\n", __func__, __LINE__, strHash);
+                return INVALID_PARAMETER;
+            }
+
+            std::string header;
+            int ret = getheader(pblockindex, header);
+            if (ret != OK)
+            {
+                return ret;
+            }
+
+            headers.push_back(header);
+        }
+
+        sendBlockHeaders(headers, WsEvent::MSG_RESPONSE, clientRequestId);
+
+        return OK;
+    }
+
     int sendNodeStaticConfiguration(const std::string& clientRequestId) {
         int version = CLIENT_VERSION;
         int protocolVersion = PROTOCOL_VERSION;
@@ -595,19 +660,26 @@ private:
         GetProxy(NET_IPV4, p);
         std::string proxy = p.IsValid() ? p.proxy.ToStringIPPort() : std::string();
         std::string errors = GetWarnings("statusbar");
-        int blocks = (int)chainActive.Height();
-        int connections = (int)vNodes.size();
+        int blocks;
         bool testnet = Params().TestnetToBeDeprecatedFieldRPC();
-        double difficulty = (double)GetDifficulty();
-        double networkDifficulty = (double)GetNetworkDifficulty();
+        double difficulty;
+        double networkDifficulty;
+        int connections;
 		std::list<std::string> peers;
         {
 			LOCK(cs_vNodes);
+	        connections = (int)vNodes.size();
 			BOOST_FOREACH(CNode* pnode, vNodes) {
 				CNodeStats stats;
 				pnode->copyStats(stats);
 				peers.push_back(stats.addrName);
 			}
+        }
+        {
+        	LOCK(cs_main);
+        	blocks = (int)chainActive.Height();
+        	difficulty = (double)GetDifficulty();
+        	networkDifficulty = (double)GetNetworkDifficulty();
         }
         sendNodeConf(version, protocolVersion, timeoffset, relayfee, proxy, errors, blocks, connections, testnet, difficulty, networkDifficulty,
             peers, WsEvent::MSG_RESPONSE, clientRequestId);
@@ -715,7 +787,7 @@ private:
             if (ec == websocket::error::closed || ec == websocket::error::no_connection)
             {
                 // graceful disconnection
-                LogPrint("ws", "%s():%d - err[%d]: %s\n", __func__, __LINE__,ec.value(), ec.message());
+                LogPrint("ws", "%s():%d - code[%d]: %s\n", __func__, __LINE__,ec.value(), ec.message());
                 return READ_ERROR;
             }
             else
@@ -738,6 +810,12 @@ private:
             msgType         = findFieldValue("msgType", request);
             clientRequestId = findFieldValue("requestId", request);
             requestType     = findFieldValue("requestType", request);
+
+            if (msgType != std::to_string(WsEvent::MSG_REQUEST)) {
+                // just log it and assume it is a request
+                LogPrint("ws", "%s():%d - WARNING: msgType[%d] invalid, assuming MSG_REQUEST (%d)\n",
+                    __func__, __LINE__, msgType, WsEvent::MSG_REQUEST);
+            }
 
             if (requestType.empty()) {
                 LogPrint("ws", "%s():%d - requestType empty: msg[%s]\n", __func__, __LINE__, msg);
@@ -837,20 +915,27 @@ private:
                 }
                 return sendHashFromLocator(hashes, strLen, clientRequestId);
             }
-            if (requestType == std::to_string(WsEvent::GET_RAW_MEMPOOL))
+            if (requestType == std::to_string(WsEvent::GET_MULTIPLE_BLOCK_HEADERS))
             {
-                reqType = WsEvent::GET_RAW_MEMPOOL;
+                reqType = WsEvent::GET_MULTIPLE_BLOCK_HEADERS;
                 if (clientRequestId.empty()) {
                     LogPrint("ws", "%s():%d - clientRequestId empty: msg[%s]\n", __func__, __LINE__, msg);
                     return MISSING_REQID;
                 }
                 const UniValue& reqPayload = find_value(request, "requestPayload");
-                if (reqPayload.isNull()) {
+                if (reqPayload.isNull())
+                {
                     LogPrint("ws", "%s():%d - requestPayload null: msg[%s]\n", __func__, __LINE__, msg);
                     return INVALID_JSON_FORMAT;
                 }
 
-                return sendRawMempool(clientRequestId);
+                const UniValue&  hashArray = find_value(reqPayload, "hashes");
+                if (hashArray.isNull()) {
+                    LogPrint("ws", "%s():%d - locatorHash empty: msg[%s]\n", __func__, __LINE__, msg);
+                    return MISSING_PARAMETER;
+                }
+
+                return sendHeadersFromHashes(hashArray, clientRequestId);
             }
             if (requestType == std::to_string(WsEvent::GET_MEMPOOL_TXS))
             {
@@ -911,6 +996,21 @@ private:
                 }
 
                 return sendEstimateFee(strBlock, clientRequestId);
+            }
+            if (requestType == std::to_string(WsEvent::GET_RAW_MEMPOOL))
+            {
+                reqType = WsEvent::GET_RAW_MEMPOOL;
+                if (clientRequestId.empty()) {
+                    LogPrint("ws", "%s():%d - clientRequestId empty: msg[%s]\n", __func__, __LINE__, msg);
+                    return MISSING_REQID;
+                }
+                const UniValue& reqPayload = find_value(request, "requestPayload");
+                if (reqPayload.isNull()) {
+                    LogPrint("ws", "%s():%d - requestPayload null: msg[%s]\n", __func__, __LINE__, msg);
+                    return INVALID_JSON_FORMAT;
+                }
+
+                return sendRawMempool(clientRequestId);
             }
             // if we are here that means it is no valid request type, and reqType is an enum defaulting to 255
             *((int*)(&reqType)) = std::stoi(requestType);
@@ -979,9 +1079,9 @@ private:
                 LogPrint("ws", "%s():%d - allocated %p\n", __func__, __LINE__, wse);
                 UniValue* rv = wse->getPayload();
                 if (!clientRequestId.empty())
-                    rv->pushKV("requestId", clientRequestId);
-                rv->pushKV("errorCode", res);
-                rv->pushKV("message", msgError);
+                    rv->push_back(Pair("requestId", clientRequestId));
+                rv->push_back(Pair("errorCode", res));
+                rv->push_back(Pair("message", msgError));
                 wsq.push(wse);
             }
         }
@@ -1144,7 +1244,16 @@ static int getblock(const CBlockIndex *pindex, std::string& strHex)
     return WsHandler::OK;
 }
 
-
+static int getheader(const CBlockIndex *pindex, std::string& strHex)
+{
+    CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+    {
+        LOCK(cs_main);
+        ss << pindex->GetBlockHeader();
+        strHex = HexStr(ss.begin(), ss.end());
+    }
+    return WsHandler::OK;
+}
 
 
 static void ws_updatetip(const CBlockIndex *pindex)
