9f9def9dfdcd928c107126daad1862f85137ea07
cronicc <cronic@zensystem.io>
2021-06-14 07:54:55 +0000

    Set preliminary fork activation heights (well into the future)

--- src/zen/forks/fork8_sidechainfork.cpp
+++ src/zen/forks/fork8_sidechainfork.cpp
@@ -4,9 +4,9 @@ namespace zen {
 
 SidechainFork::SidechainFork()
 {
-    setHeightMap({{CBaseChainParams::Network::MAIN,999999}, // TODO set it properly
+    setHeightMap({{CBaseChainParams::Network::MAIN,1111111}, // TODO set it properly
                   {CBaseChainParams::Network::REGTEST,420},
-                  {CBaseChainParams::Network::TESTNET,750000}}); //MOVED UP TODO set it properly
+                  {CBaseChainParams::Network::TESTNET,900000}}); // TODO set it properly
 
 }
 
