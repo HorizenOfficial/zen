4142efd5ec6efed2a07b1a13b0b7884080902b45
Alberto Sala <alberto.sala@nttdata.com>
2021-06-15 12:24:15 +0200

    Adapted sc_big_block-py to new sc fork height

--- qa/rpc-tests/sc_big_block.py
+++ qa/rpc-tests/sc_big_block.py
@@ -5,6 +5,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.authproxy import JSONRPCException
+from test_framework.test_framework import MINIMAL_SC_HEIGHT
 from test_framework.util import assert_true, assert_equal, initialize_chain_clean, \
     start_nodes, stop_nodes, wait_bitcoinds, sync_blocks, sync_mempools, connect_nodes_bi, mark_logs, \
     dump_sc_info, dump_sc_info_record, get_epoch_data, get_spendable, swap_bytes, advance_epoch
@@ -39,7 +40,7 @@ class sc_big_block(BitcoinTestFramework):
 
         self.nodes = start_nodes(NUMB_OF_NODES, self.options.tmpdir,
                                  extra_args=[['-logtimemicros=1', '-debug=py', '-debug=net',
-                                              #'-debug=sc', '-debug=mempool', '-debug=cert', '-par=5'
+                                              #'-debug=sc', '-debug=mempool', '-debug=cert', '-par=5',
                                               '-debug=bench']] * NUMB_OF_NODES)
 
         connect_nodes_bi(self.nodes, 0, 1)
@@ -121,8 +122,8 @@ class sc_big_block(BitcoinTestFramework):
         self.nodes[1].generate(2)
         self.sync_all()
 
-        mark_logs("Node 0 generates 220 block",self.nodes,DEBUG_MODE)
-        self.nodes[0].generate(220)
+        mark_logs("Node 0 generates {} block".format(MINIMAL_SC_HEIGHT), self.nodes, DEBUG_MODE)
+        self.nodes[0].generate(MINIMAL_SC_HEIGHT)
         self.sync_all()
 
         #generate Vks and constant
