f4be48201bbaef07d55f9c51f0eb176a1e82fe51
Alberto Sala <alberto.sala@nttdata.com>
2021-06-17 18:57:13 +0200

    Changed size limit for cert/csw vks and proofs according to issue #123; aligned with mc-crypto-lib on current head of branch priority_verification

--- depends/packages/libzendoo.mk
+++ depends/packages/libzendoo.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1.0
 $(package)_download_path=https://github.com/HorizenOfficial/zendoo-mc-cryptolib/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=48469539c2e05f1b18b8a6f5ad42a5306bd02d2a2ec5a2855b0fbe5f8b979ac6
-$(package)_git_commit=c087be2f26f8c0a61c57a72c2e327e40ac2a1172
+$(package)_sha256_hash=4efd1e7bd30214e2bfb3c35a5047799c00a18dcb63fdb467a9dd0ccde01d36b6
+$(package)_git_commit=705f7263caf305b1f9927e2e82fc52f6e4197926
 $(package)_dependencies=rust $(rust_crates_z)
 $(package)_patches=cargo.config
 
