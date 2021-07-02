01c8accb208c4bbc885d7de72d37824f1a7fb555
DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
2021-07-02 13:36:06 +0200

    Update libzendoo.mk

--- depends/packages/libzendoo.mk
+++ depends/packages/libzendoo.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1.0
 $(package)_download_path=https://github.com/HorizenOfficial/zendoo-mc-cryptolib/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=5fdb01692ff39addf185bba925743f88648f98d3dcc69f37a1d818ca0b9c6891
-$(package)_git_commit=76bf2a3cbc8cd7beea588486587ca3cb59f58f54
+$(package)_sha256_hash=c9352e9bd27125c7bd5f7f061014fec669903155dac8841420aa2b7d0202b93f
+$(package)_git_commit=3f8cc929b0e6866d2ed81972db053ac233a2507c
 $(package)_dependencies=rust $(rust_crates_z)
 $(package)_patches=cargo.config
 
