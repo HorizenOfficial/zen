60b0b51608ec6c8d4a4ca7c256ca38839b4d437d
cronicc <cronic@zensystem.io>
2021-10-04 17:53:03 +0000

    Pull in patch fixing OpenSSL build error on older MacOS: https://github.com/openssl/openssl/commit/96ac8f13f4d0ee96baf5724d9f96c44c34b8606c

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -3,6 +3,7 @@ $(package)_version=1.1.1l
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
+$(package)_patches=0001-Darwin-platform-allows-to-build-on-releases-before-Y.patch
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
@@ -93,7 +94,8 @@ endef
 
 define $(package)_preprocess_cmds
   sed -i.old 's/built on: $$$$date/built on: date not available/' util/mkbuildinf.pl && \
-  sed -i.old "s|\"engines\", \"apps\", \"test\"|\"engines\"|" Configure
+  sed -i.old "s|\"engines\", \"apps\", \"test\"|\"engines\"|" Configure && \
+  patch -p1 < $($(package)_patch_dir)/0001-Darwin-platform-allows-to-build-on-releases-before-Y.patch
 endef
 
 define $(package)_config_cmds
