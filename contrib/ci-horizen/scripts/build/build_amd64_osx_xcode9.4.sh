42140590f89c52b093f9898d6a44d86f32b10f77
cronicc <cronic@zensystem.io>
2021-11-05 14:39:35 +0000

    Lint ci shell scripts

--- contrib/ci-horizen/scripts/build/build_amd64_osx_xcode9.4.sh
+++ contrib/ci-horizen/scripts/build/build_amd64_osx_xcode9.4.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 cd "${B2_DL_DECOMPRESS_FOLDER}"
+# shellcheck disable=SC1091
 source environment
 
 cd "${TRAVIS_BUILD_DIR}"
@@ -11,4 +12,5 @@ export LIBTOOLIZE=glibtoolize
 # travis-ci has a 4MB log size limitation after which any job is terminated
 # due to stdout verbosity we only display stderr
 # stdout and stderr are saved in build.log, the last 100 lines are displayed on error
+# shellcheck disable=SC2086
 time ./zcutil/build-mac-clang.sh $MAKEFLAGS > >(tee -a "${TRAVIS_BUILD_DIR}/build.log" > /dev/null) 2> >(tee -a "${TRAVIS_BUILD_DIR}/build.log" >&2) || (tail -n 100 "${TRAVIS_BUILD_DIR}/build.log"; false)
--- contrib/ci-horizen/scripts/common/setup_environment.sh
+++ contrib/ci-horizen/scripts/common/setup_environment.sh
@@ -24,21 +24,26 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   if [ -z "${TRAVIS_TAG}" ]; then
     export IMAGE_TAG="${IMAGE_LATEST_TAG}"
   else
-    export IMAGE_TAG="${IMAGE_BASE_TAG}-$(echo -n ${TRAVIS_TAG} | sed 's/[^-._a-zA-Z0-9]/-/g')"
+    # shellcheck disable=SC2001,SC2155
+    export IMAGE_TAG="${IMAGE_BASE_TAG}-$(sed 's/[^-._a-zA-Z0-9]/-/g' <<< "${TRAVIS_TAG}")"
   fi
   if [ "${TRAVIS_BUILD_STAGE_NAME}" = "Prepare" ]; then
-    export DATE=$(date '+%Y-%m-%d')
+    # shellcheck disable=SC2155
+    export DATE="$(date '+%Y-%m-%d')"
     export IMAGE_PATH="${TRAVIS_BUILD_DIR}/contrib/ci-horizen/dockerfiles/${DOCKER_ARCH}/${DOCKER_TARGET_OS}/${DOCKER_FROM}"
     NEED_DOCKER_CREDS="true"
   fi
   if [ "${TRAVIS_BUILD_STAGE_NAME}" = "Build" ]; then
     export B2_UL_COMPRESS_FOLDER="${DOCKER_HOME}"
-    export B2_UL_FILENAME="${DOCKER_ARCH}-${DOCKER_TARGET_OS}-${DOCKER_FROM}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< ${MAKEFLAGS}).tar.gz"
+    # shellcheck disable=SC2001,SC2155
+    export B2_UL_FILENAME="${DOCKER_ARCH}-${DOCKER_TARGET_OS}-${DOCKER_FROM}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< "${MAKEFLAGS}").tar.gz"
     NEED_B2_CREDS="true"
   fi
   if [ "${TRAVIS_BUILD_STAGE_NAME}" = "Test" ]; then
-    export B2_DL_DECOMPRESS_FOLDER="${DOCKER_HOME}/$(basename ${TRAVIS_BUILD_DIR})"
-    export B2_DL_FILENAME="${DOCKER_ARCH}-${DOCKER_TARGET_OS}-${DOCKER_FROM}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< ${MAKEFLAGS}).tar.gz"
+    # shellcheck disable=SC2155
+    export B2_DL_DECOMPRESS_FOLDER="${DOCKER_HOME}/$(basename "${TRAVIS_BUILD_DIR}")"
+    # shellcheck disable=SC2001,SC2155
+    export B2_DL_FILENAME="${DOCKER_ARCH}-${DOCKER_TARGET_OS}-${DOCKER_FROM}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< "${MAKEFLAGS}").tar.gz"
   fi
   if [ "${TRAVIS_BUILD_STAGE_NAME}" = "Package" ]; then
     NEED_B2_CREDS="true"
@@ -79,13 +84,15 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
     export B2_DL_DECOMPRESS_FOLDER="${HOME}/zencash-apple"
     export B2_DL_FILENAME="${TRAVIS_CPU_ARCH}-${TRAVIS_OS_NAME}-${TRAVIS_OSX_IMAGE}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}-zencash-apple.tar.gz"
     export B2_UL_COMPRESS_FOLDER="${TRAVIS_BUILD_DIR}"
-    export B2_UL_FILENAME="${TRAVIS_CPU_ARCH}-${TRAVIS_OS_NAME}-${TRAVIS_OSX_IMAGE}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< ${MAKEFLAGS}).tar.gz"
+    # shellcheck disable=SC2001,SC2155
+    export B2_UL_FILENAME="${TRAVIS_CPU_ARCH}-${TRAVIS_OS_NAME}-${TRAVIS_OSX_IMAGE}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< "${MAKEFLAGS}").tar.gz"
     NEED_B2_CREDS="true"
   fi
   if [ "${TRAVIS_BUILD_STAGE_NAME}" = "Test" ]; then
     export PIP_INSTALL="${PIP_INSTALL} pyblake2 pyzmq websocket-client2"
     export B2_DL_DECOMPRESS_FOLDER="${TRAVIS_BUILD_DIR}"
-    export B2_DL_FILENAME="${TRAVIS_CPU_ARCH}-${TRAVIS_OS_NAME}-${TRAVIS_OSX_IMAGE}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< ${MAKEFLAGS}).tar.gz"
+    # shellcheck disable=SC2001,SC2155
+    export B2_DL_FILENAME="${TRAVIS_CPU_ARCH}-${TRAVIS_OS_NAME}-${TRAVIS_OSX_IMAGE}-${TRAVIS_BUILD_ID}-${TRAVIS_COMMIT}$(sed 's/ //g' <<< "${MAKEFLAGS}").tar.gz"
     mkdir -p "$HOME/ZcashParams"
     ln -sf "$HOME/ZcashParams" "$HOME/Library/Application Support/ZcashParams"
   fi
