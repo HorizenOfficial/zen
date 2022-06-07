856ab0ab567368ef3f3f28a66662eec112e3c5d0
Daniele Rogora <danielerogora@horizenlabs.io>
2022-06-07 12:24:25 +0200

    Update zeromq to version 4.3.4.
    This fixes compilation on clang >= 10.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,16 +1,24 @@
 package=zeromq
-$(package)_version=4.3.1
+$(package)_version=4.3.4
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
+$(package)_sha256_hash=c593001a89f5a85dd2ddf564805deb860e02471171b3f204944857336295c3e5
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-documentation --disable-shared --disable-curve
+  $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf
+  $(package)_config_opts += --without-libgssapi_krb5 --without-pgm --without-norm --without-vmci
+  $(package)_config_opts += --disable-libunwind --disable-radix-tree --without-gcov --disable-dependency-tracking
+  $(package)_config_opts += --disable-drafts --enable-option-checking
   $(package)_config_opts_linux=--with-pic
-  $(package)_cxxflags=-std=c++17
+  $(package)_config_opts_freebsd=--with-pic
+  $(package)_config_opts_mingw32=--disable-Werror
+  $(package)_cxxflags+=-std=c++17
 endef
 
+# calling autogen.sh is necessary to compile on systems with aclocal < 1.16 (e.g. Ubuntu bionic).
+# Can be removed if not needed by the CI.
 define $(package)_config_cmds
+  ./autogen.sh && \
   $($(package)_autoconf)
 endef
 
@@ -23,5 +31,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf bin share
+  rm -rf bin share lib/*.la
 endef
