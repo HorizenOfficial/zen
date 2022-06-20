01ebdbfb657ffdaa6f154b44a8add9a1b479a452
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2022-06-20 15:22:51 +0200

    Remove inclusions and exclusions through grep from the linting scripts
    We switched to using the configuration parameters supported by cppcheck and OcLint to filter the output.

--- qa/lint/extended-lint-cppcheck.sh
+++ qa/lint/extended-lint-cppcheck.sh
@@ -12,20 +12,5 @@ if ! command -v cppcheck > /dev/null; then
     exit 0
 fi
 
-function join_array {
-    local IFS="$1"
-    shift
-    echo "$*"
-}
-
 # cppcheck-suppressions-list.txt is required since "-i" only ignores source files, but not headers, see "cppcheck --help" for further details.
-WARNINGS=$(cppcheck --enable=all -j "$(getconf _NPROCESSORS_ONLN)" --language=c++ --std=c++17 --template=gcc --project=compile_commands.json -q -isrc/leveldb -isrc/secp256k1 -isrc/snark -isrc/univalue --suppressions-list=qa/lint/cppcheck-suppressions-list.txt 2>&1)
-if [[ ${WARNINGS} != "" ]]; then
-    echo "${WARNINGS}"
-    echo
-    echo "Advice not applicable in this specific case? Add an exception by updating"
-    echo "IGNORED_WARNINGS in $0"
-    # Uncomment to enforce the developer note policy "By default, declare single-argument constructors `explicit`"
-    # exit 1
-fi
-exit 0
+cppcheck --enable=all -j "$(getconf _NPROCESSORS_ONLN)" --language=c++ --std=c++17 --template=gcc --project=compile_commands.json -q -isrc/leveldb -isrc/secp256k1 -isrc/snark -isrc/univalue --suppressions-list=qa/lint/cppcheck-suppressions-list.txt 2>&1
--- qa/lint/extended-lint-oclint.sh
+++ qa/lint/extended-lint-oclint.sh
@@ -7,38 +7,12 @@
 
 export LC_ALL=C
 
-ENABLED_CHECKS=(
-    "Class '.*' has a constructor with 1 argument that is not explicit."
-    "Struct '.*' has a constructor with 1 argument that is not explicit."
-)
-
-IGNORED_WARNINGS=(
-    ".*long line \[size\|P3\] Line with .* characters exceeds limit of .*"
-    ".*long variable name \[naming\|P3\] Length of variable name .* is .*, which is longer than the threshold of .*"
-)
-
 if ! command -v oclint > /dev/null; then
     echo "Skipping oclint linting since oclint is not installed."
     exit 0
 fi
 
-function join_array {
-    local IFS="$1"
-    shift
-    echo "$*"
-}
 
-ENABLED_CHECKS_REGEXP=$(join_array "|" "${ENABLED_CHECKS[@]}")
-IGNORED_WARNINGS_REGEXP=$(join_array "|" "${IGNORED_WARNINGS[@]}")
-WARNINGS=$(oclint-json-compilation-database -e src/leveldb -e src/secp256k1 -e src/univalue -e src/snark -e depends 2>&1)
-    # grep -E "$ENABLED_CHECKS_REGEXP" | \
-    # grep -vE "${IGNORED_WARNINGS_REGEXP}")
-if [[ ${WARNINGS} != "" ]]; then
-    echo "${WARNINGS}"
-    echo
-    echo "Advice not applicable in this specific case? Add an exception by updating"
-    echo "IGNORED_WARNINGS in $0"
-    # Uncomment to enforce the developer note policy "By default, declare single-argument constructors `explicit`"
-    # exit 1
-fi
-exit 0
+# To disable specific rules, use "-- -disable-rule=<rule name", for example "-- -disable-rule=LongLine".
+# The list of all rules can be found by running "oclint . -list-enabled-rules".
+oclint-json-compilation-database -e src/leveldb -e src/secp256k1 -e src/univalue -e src/snark -e depends 2>&1
