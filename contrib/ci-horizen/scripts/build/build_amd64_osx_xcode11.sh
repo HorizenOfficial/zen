0a630b04b3c5c902a2371c5422f9bc652ccede75
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-10-06 10:21:11 +0200

    Add osx script for xCode 11

---
+++ contrib/ci-horizen/scripts/build/build_amd64_osx_xcode11.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "${B2_DL_DECOMPRESS_FOLDER}"
+# shellcheck disable=SC1091
+source environment
+
+cd "${TRAVIS_BUILD_DIR}"
+export MAKEFLAGS="${MAKEFLAGS:-} -j $(($(nproc)+1))"
+export LIBTOOLIZE=glibtoolize
+# travis-ci has a 4MB log size limitation after which any job is terminated
+# due to stdout verbosity we only display stderr
+# stdout and stderr are saved in build.log, the last 100 lines are displayed on error
+# shellcheck disable=SC2086
+time ./zcutil/build-mac-clang.sh $MAKEFLAGS > >(tee -a "${TRAVIS_BUILD_DIR}/build.log" > /dev/null) 2> >(tee -a "${TRAVIS_BUILD_DIR}/build.log" >&2) || (tail -n 100 "${TRAVIS_BUILD_DIR}/build.log"; false)
