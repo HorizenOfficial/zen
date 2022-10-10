b76336f7c080d0c114be29ba7d40ae2e2a1cb3b4
Alessandro Petrini <apetrini@horizenlabs.io>
2022-10-10 11:08:47 +0200

    added shebang to Python test

--- qa/rpc-tests/sc_ft_and_mbtr_fees_update.py
+++ qa/rpc-tests/sc_ft_and_mbtr_fees_update.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014 The Bitcoin Core developers
+# Copyright (c) 2018 The Zencash developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.authproxy import JSONRPCException
 from test_framework.util import assert_equal, assert_greater_than, assert_true, initialize_chain_clean, \
