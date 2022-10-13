6daf61c99e315d9f4d9e61d23d278171d9b379bf
Daniele Rogora <danielerogora@horizenlabs.io>
2022-10-13 12:43:09 +0200

    Fix warnings/errors generated by recent GCC/clang.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -3,7 +3,7 @@ $(package)_version=1_74_0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1
-$(package)_patches=signals2-noise.patch
+$(package)_patches=signals2-noise.patch nonnull.patch
 
 $(package)_compiler=
 ifeq ($(CLANG_ARG),true)
@@ -34,7 +34,8 @@ $(package)_cxxflags_freebsd=-fPIC
 endef
 
 define $(package)_preprocess_cmds
-  patch -p2 < $($(package)_patch_dir)/signals2-noise.patch
+  patch -p2 < $($(package)_patch_dir)/signals2-noise.patch && \
+  patch -p2 < $($(package)_patch_dir)/nonnull.patch
 endef
 
 define $(package)_config_cmds
