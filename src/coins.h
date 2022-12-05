ffbe622e0218d669de3e303f9cbb0f9448b089f6
Daniele Rogora <danielerogora@horizenlabs.io>
2022-12-05 18:20:38 +0100

    Revert multiple certificates in mempool
    This commit reverts previous modifications introduced to handle multiple certificates for a single sidechain in a single
    block. This also required handling such certificates in the same mempool.

--- src/coins.h
+++ src/coins.h
@@ -699,7 +699,7 @@ public:
     bool RevertTxOutputs(const CTransaction& tx, int nHeight);
 
     //CERTIFICATES RELATED PUBLIC MEMBERS
-    CValidationState::Code IsCertApplicableToState(const CScCertificate& cert, bool skipTiming) const;
+    CValidationState::Code IsCertApplicableToState(const CScCertificate& cert) const;
 
     CValidationState::Code CheckEndEpochCumScTxCommTreeRoot(
         const CSidechain& sidechain, int epochNumber, const CFieldElement& endCumScTxCommTreeRoot, int &referencedHeight) const;
