9afb8e37a8585f1a569c7012b975a474d7276003
la10736 <michele.damico@gmail.com>
2023-02-06 11:49:40 +0100

    Remove useless crate dependencies. Now we use cargo registry cache.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -37,78 +37,9 @@ rust_crates := \
   crate_winapi_x86_64_pc_windows_gnu \
   crate_zip32
 
-rust_crates_z := \
-    crate_adler_z \
-    crate_bit_vec_z \
-    crate_blake2_z \
-    crate_bzip2_z \
-    crate_bzip2_sys_z \
-    crate_byte_tools_z \
-    crate_cc_z \
-    crate_crc32fast_z \
-    crate_crypto_mac_z \
-    crate_digest_z \
-    crate_flate2_z \
-	crate_rand_z \
-	crate_lazy_static_z \
-	crate_libc_z \
-	crate_winapi_z \
-	crate_getrandom_z \
-	crate_maybe-uninit_z \
-	crate_c2-chacha_z \
-	crate_cfg_if_z \
-	crate_cloudabi_z \
-	crate_crossbeam-channel_z \
-	crate_crossbeam-deque_z \
-	crate_crossbeam-queue_z \
-	crate_crossbeam-epoch_z \
-	crate_crossbeam-utils_z \
-	crate_derivative_z \
-	crate_generic_array_z \
-	crate_memoffset_z \
-	crate_hex_z \
-	crate_syn_z \
-	crate_byteorder_z \
-	crate_either_z \
-    crate_miniz_oxide_z \
-	crate_num_cpus_z \
-	crate_opaque_debug_z \
-	crate_ppv-lite86_z \
-	crate_quote_z \
-	crate_rand_core_z \
-	crate_rand_chacha_z \
-	crate_rayon_z \
-	crate_rayon-core_z \
-	crate_scopeguard_z \
-	crate_smallvec_z \
-	crate_pkg-config_z \
-	crate_proc-macro2_z \
-	crate_autocfg_z \
-	crate_rand_hc_z \
-	crate_semver_z \
-	crate_semver-parser_z \
-	crate_serde_z \
-	crate_serde_derive_z \
-	crate_sha1_z \
-	crate_subtle_z \
-	crate_typenum_z \
-	crate_unroll_z \
-	crate_unicode_xid_z \
-	crate_winapi_i686_pc_windows_gnu \
-	crate_winapi_x86_64_pc_windows_gnu \
-	crate_wasi \
-	crate_hermit-abi_z \
-	crate_radix_trie_z \
-	crate_rustc_version_z \
-	crate_endian-type_z \
-	crate_nibble_vec_z \
-	crate_num_bigint_z \
-	crate_num_integer_z \
-	crate_num_traits_z
-	
 #rust_packages := rust $(rust_crates) 
 rust_packages := rust $(rust_crates) librustzcash
-rust_packages_zendoo := rust $(rust_crates_z) libzendoo
+rust_packages_zendoo := rust libzendoo
 proton_packages := proton
 zcash_packages := libgmp libsodium
 packages := boost openssl libevent zeromq $(zcash_packages) googletest
