404384a968571cf98a8835bf17b80f6bd2a9acd3
cronicc <cronic@zensystem.io>
2023-02-08 22:19:44 +0000

    Change testnet default port to 20033

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -194,7 +194,7 @@ public:
         pchMessageStart[2] = 0xcd;
         pchMessageStart[3] = 0xe6;
         vAlertPubKey = ParseHex("048679fb891b15d0cada9692047fd0ae26ad8bfb83fabddbb50334ee5bc0683294deb410be20513c5af6e7b9cec717ade82b27080ee6ef9a245c36a795ab044bb3");
-        nDefaultPort = 19033;
+        nDefaultPort = 20033;
 //        nMinerThreads = 0;
         nPruneAfterHeight = 1000;
 
