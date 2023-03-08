3b3f5b86a9a84b31fba273dda7fb5f26a0c664a4
GiacomoPirinoli <gpirinoli@horizenlabs.io>
2023-03-08 17:41:13 +0100

    Completed test for checking consensus rule

--- qa/rpc-tests/zcjoinsplit.py
+++ qa/rpc-tests/zcjoinsplit.py
@@ -4,10 +4,11 @@
 # Test joinsplit semantics
 #
 
-from test_framework.test_framework import BitcoinTestFramework, ForkHeights
+from test_framework.test_framework import BitcoinTestFramework, ForkHeights, sync_blocks
 from test_framework.authproxy import JSONRPCException
 from test_framework.util import assert_equal, start_node, \
-    gather_inputs
+    gather_inputs, connect_nodes, disconnect_nodes
+from decimal import Decimal
 
 import sys
 
@@ -18,6 +19,8 @@ class JoinSplitTest(BitcoinTestFramework):
         self.nodes = []
         self.is_network_split = False
         self.nodes.append(start_node(0, self.options.tmpdir))
+        self.nodes.append(start_node(1, self.options.tmpdir))
+        connect_nodes(self.nodes[0], 1)
 
     def run_test(self):
 
@@ -25,34 +28,58 @@ class JoinSplitTest(BitcoinTestFramework):
 
         # first round pre-fork, second round post-fork
         pre_fork_round = 0
-        post_fork_round = 1
-        for round in range(2):
+        just_pre_fork_round = 1
+        post_fork_round = 2
+        for round in range(3):
             zckeypair = self.nodes[0].zcrawkeygen()
             zcsecretkey = zckeypair["zcsecretkey"]
             zcaddress = zckeypair["zcaddress"]
 
             (total_in, inputs) = gather_inputs(self.nodes[0], 45.75)
+
             protect_tx = self.nodes[0].createrawtransaction(inputs, {})
-            joinsplit_result = self.nodes[0].zcrawjoinsplit(protect_tx, {}, {zcaddress:45.74}, 45.74, 0)
+            joinsplit_result = self.nodes[0].zcrawjoinsplit(protect_tx, {}, {zcaddress: total_in - Decimal(0.01)}, total_in - Decimal(0.01), 0)
 
             receive_result = self.nodes[0].zcrawreceive(zcsecretkey, joinsplit_result["encryptednote1"])
             assert_equal(receive_result["exists"], False)
 
             protect_tx = self.nodes[0].signrawtransaction(joinsplit_result["rawtxn"])
+
+            # disconnect the nodes so that the chains are not synced
+            if (round == just_pre_fork_round):
+                disconnect_nodes(self.nodes[0], 1)
+
             try:
                 self.nodes[0].sendrawtransaction(protect_tx["hex"])
                 if (round == post_fork_round):
                     assert(False)
             except JSONRPCException as e:
-                if (round == pre_fork_round):
+                if (round == pre_fork_round or round == just_pre_fork_round):
                     print("Unexpected exception caught during testing: " + str(sys.exc_info()[0]))
                     assert(False)
                 else:
                     print("Expected exception caught during testing due to deprecation (error=" + str(e.error["code"]) + ")")
                     assert_equal(e.error["code"], RPC_VERIFY_REJECTED)
-                    break
+                    continue # other steps of this round are skipped
 
-            # HERE SHOULD ADD A CHECK ON JUST THE MINING (NOT MEMPOOL)
+            if (round == just_pre_fork_round):
+                # advance chain on node1 (so that shielded pool deprecation hard fork is active)
+                self.nodes[1].generate(20)
+                connect_nodes(self.nodes[0], 1)
+                sync_blocks(self.nodes)
+                # make sure now deprecated tx is node0 mempool
+                assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
+                # mine a block including now deprecated tx
+                try:
+                    self.nodes[0].generate(1)
+                    assert(False)
+                except JSONRPCException as e:
+                    # failing at CreateNewBlock->TestBlockValidity
+                    # (very close to block validation failure a node receiving this block would incur into)
+                    print("Expected exception caught during testing due to deprecation (error=" + str(e.error["code"]) + ")")
+                    self.nodes[0].clearmempool()
+                    self.sync_all()
+                    continue # other steps of this round are skipped
 
             self.nodes[0].generate(1)
 
@@ -78,8 +105,8 @@ class JoinSplitTest(BitcoinTestFramework):
 
             if (round == pre_fork_round):
                 blockcount = self.nodes[0].getblockcount()
-                if (blockcount < ForkHeight):
-                    self.nodes[0].generate(ForkHeight - blockcount)
+                if (blockcount < ForkHeight - 10):
+                    self.nodes[0].generate(ForkHeight - 10 - blockcount)
                     self.sync_all()
 
 
