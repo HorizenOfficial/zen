3b845c53b648aef9fdaaf9d8650ba579142d90be
Daniele Rogora <danielerogora@horizenlabs.io>
2023-03-15 18:28:27 +0100

    Update gtest to 1.13

--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -1,9 +1,9 @@
 package=googletest
-$(package)_version=1.8.0
-$(package)_download_path=https://github.com/google/$(package)/archive/
+$(package)_version=1.13.0
+$(package)_download_path=https://github.com/google/$(package)/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_download_file=release-$($(package)_version).tar.gz
-$(package)_sha256_hash=58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
+$(package)_download_file=v$($(package)_version).tar.gz
+$(package)_sha256_hash=ad7fdba11ea011c1d925b3289cf4af2c66a352e18d4c7264392fead75e919363
 
 define $(package)_set_vars
 $(package)_cxxflags+=-std=c++17
@@ -11,14 +11,16 @@ $(package)_cxxflags_linux=-fPIC
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C googlemock/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gmock.a && \
-  $(MAKE) -C googletest/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gtest.a
+  mkdir -p ./build && \
+  cd ./build && \
+  cmake .. && \
+  $(MAKE) CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)"
 endef
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_dir)$(host_prefix)/lib && \
-  install ./googlemock/make/gmock.a $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
-  install ./googletest/make/gtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
+  install ./build/lib/libgmock.a $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
+  install ./build/lib/libgtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
   cp -a ./googlemock/include $($(package)_staging_dir)$(host_prefix)/ && \
   cp -a ./googletest/include $($(package)_staging_dir)$(host_prefix)/
 endef
