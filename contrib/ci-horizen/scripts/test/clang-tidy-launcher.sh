542d69513a249184abfdd88e0a7ab77225e2bfe0
Alessandro Petrini <apetrini@horizenlabs.io>
2023-03-28 18:09:44 +0200

    ci: improve clang-tidy launcher
    - Clang-tidy executed on all the files of the repository
    - No header filter regex, as it slowed down processing up to 4x
    - Add additional grep to reduce the size of reports generated by clang-tidy, due to the lack
      of header filtering. Remove all the warnings coming from external libraries (Boost, etc...)
    - Add regex in codacy-clang-tidy-linux pipeline to strip all ANSI escape sequences
    - Remove optional checks on clang-tidy
    - Remove Python scripts that are now useless

--- contrib/ci-horizen/scripts/test/clang-tidy-launcher.sh
+++ contrib/ci-horizen/scripts/test/clang-tidy-launcher.sh
@@ -3,50 +3,30 @@
 # Get the current commit hash
 export COMMIT=$(git log -1 --format="%H")
 
-declare FILTEREDFILELIST
+echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+echo "  Starting code linting with clang-tidy for commit $COMMIT"
+echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+echo
 
-echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-echo "  Starting code linting with clang-tidy for commit $COMMIT."
-echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-echo ""
+echo "Analyzing all files"
 
-# Call clang-tidy_getFileList.py to extract a list of meaningful file
-originalIFS="$IFS"; IFS='|'
-read -a FILTEREDFILELIST <<< $(./contrib/ci-horizen/scripts/test/clang-tidy_getFileList.py "${CODACY_API_TOKEN_COVERAGE}" "${COMMIT}")
-IFS="$originalIFS"
-
-if [ -n "$FILTEREDFILELIST" ] ;
-then
-    echo "Analyzing the following files:"
-    for i in ${FILTEREDFILELIST[@]}
-    do
-        echo "${i}"
-    done
-    echo ""
-
-    declare headerList
-    # Build the list of header file to be included in the analysis. Note that currently the logic
-    # behind '-header-filter' regex is broken, so it is not possible to exclude a path.
-    # The purpose of this workaround is to avoid analyzing the majority of headers belonging to
-    # external libraries, i.e. those in the ./depends folder
-    read headerList <<< $(./contrib/ci-horizen/scripts/test/clang-tidy_getHeaders.py)
-
-    # Run clang-tidy on the filtered list
-    # Recommended checks - amongs those available
-    # --checks='modernize*, readability*, hicpp*, cppcoreguidelines*, bugprone*, performance*'
-    run-clang-tidy -header-filter="${headerList}" -checks='cppcoreguidelines*, performance*, bugprone*' "${FILTEREDFILELIST[@]}" | \
-    # Convert the Clang-Tidy output to a format that the Codacy API accepts
-    codacy-clang-tidy-linux-1.3.7 | \
-    # Send the results to Codacy
-    curl -v -XPOST -L -H "project-token: ${CODACY_PROJECT_TOKEN}" \
-       -H "Content-type: application/json" -d @- \
-       "https://api.codacy.com/2.0/commit/${COMMIT}/issuesRemoteResults"
-else
-    # Skip clang-tidy if there are no meaningful files
-    echo "Skipping clang-tidy: no files to be analyzed"
-fi
+# Recommended checks - amongs those available
+# --checks='modernize*, readability*, hicpp*, cppcoreguidelines*, bugprone*, performance*'
+run-clang-tidy -quiet -header-filter=".*" -checks='cppcoreguidelines*' | \
+# Filter out all the ANSI escape sequences that colour the report (--use-color is hardwired in run-clang-tidy-14)
+sed -e 's/\x1b\[[0-9;]*m//g' | \
+# Select only the warning lines
+grep "warning:" | \
+# Remove all the dependencies
+grep -v "include/boost" | grep -v "include/openssl" | grep -v "include/sodium" | grep -v "include/leveldb" | grep -v "include/event2" | grep -v "include/gtest" | grep -v "include/gmock" | \
+# Convert the Clang-Tidy output to a format that the Codacy API accepts
+codacy-clang-tidy-linux-1.3.7 | \
+# Send the results to Codacy
+curl -v -XPOST -L -H "project-token: ${CODACY_PROJECT_TOKEN}" \
+    -H "Content-type: application/json" -d @- \
+    "https://api.codacy.com/2.0/commit/${COMMIT}/issuesRemoteResults"
 
 echo
-echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-echo "                     Code linting done"
-echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+echo "                                      Code linting done                                      "
+echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
