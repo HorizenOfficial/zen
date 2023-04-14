2fb9439249a2c6d78d3e0d4711d5748253eb2de3
Daniele Rogora <danielerogora@horizenlabs.io>
2023-04-14 17:41:53 +0200

    Fixup on PR537 feedback

--- qa/rpc-tests/p2p_ignore_spent_tx.py
+++ qa/rpc-tests/p2p_ignore_spent_tx.py
@@ -21,9 +21,6 @@ class old_tx_ignored(BitcoinTestFramework):
     def setup_chain(self, split=False):
         print("Initializing test directory " + self.options.tmpdir)
         initialize_chain_clean(self.options.tmpdir, NUMB_OF_NODES)
-        self.alert_filename = os.path.join(self.options.tmpdir, "alert.txt")
-        with open(self.alert_filename, 'w'):
-            pass  # Just open then close to create zero-length file
 
     def setup_network(self, split=False):
         self.nodes = []
