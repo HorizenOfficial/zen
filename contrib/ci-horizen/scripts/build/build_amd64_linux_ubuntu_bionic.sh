9b98138e7bdb23ff2c08a68a6ff864c85ec4b23f
Alessandro Petrini <apetrini@horizenlabs.io>
2023-05-08 10:29:55 +0200

    ci: removed Ubuntu Bionic prepare/build scripts from contrib/ci-horizen
    Removed Bionic Dockerfiles and entrypoint.sh scripts for Linux and Windows builds
    Added Focal Dockerfile and entrypoint script for Windows build
    Removed Bionic build scripts for Linux and Windows

--- contrib/ci-horizen/scripts/build/build_amd64_linux_ubuntu_bionic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker run --rm -v "${TRAVIS_BUILD_DIR}:/mnt/build" -v "$HOME/.ccache:/mnt/.ccache" -e LOCAL_USER_ID="$(id -u)" \
-  -e LOCAL_GRP_ID="$(id -g)" --env-file <(env | grep 'DOCKER_\|MAKEFLAGS') "${IMAGE_NAME}:${IMAGE_TAG}" \
-  bash -c 'set -xeuo pipefail && export HOST="$(gcc -dumpmachine)" && export MAKEFLAGS="${MAKEFLAGS:-} -j $(($(nproc)+1))" \
-    && cd "${DOCKER_HOME}" && /usr/bin/time -v ./zcutil/build.sh $MAKEFLAGS'
