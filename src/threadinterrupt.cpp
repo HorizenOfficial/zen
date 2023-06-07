5cddd00ed5a1c0ab54261131d27db2270aea0ad6
Alessandro Petrini <apetrini@horizenlabs.io>
2023-06-07 16:20:37 +0200

    feat: backport threadinterrupt.cpp/.h from Bitcoin

---
+++ src/threadinterrupt.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "threadinterrupt.h"
+
+CThreadInterrupt::operator bool() const
+{
+    return flag.load(std::memory_order_acquire);
+}
+
+void CThreadInterrupt::reset()
+{
+    flag.store(false, std::memory_order_release);
+}
+
+void CThreadInterrupt::operator()()
+{
+    {
+        std::unique_lock<std::mutex> lock(mut);
+        flag.store(true, std::memory_order_release);
+    }
+    cond.notify_all();
+}
+
