76b19f77239beb0dc7dc544b3fd66b3a8cf47854
GiacomoPirinoli <gpirinoli@horizenlabs.io>
2023-06-09 18:37:54 +0200

    Hardcoded fork activation height (private testnet)

--- src/zen/forks/fork11_shieldedpooldeprecationfork.cpp
+++ src/zen/forks/fork11_shieldedpooldeprecationfork.cpp
@@ -6,6 +6,6 @@ ShieldedPoolDeprecationFork::ShieldedPoolDeprecationFork()
 {
     setHeightMap({{CBaseChainParams::Network::MAIN,2500001},            // TODO: MODIFY PLACEHOLDER!
                   {CBaseChainParams::Network::REGTEST,990},             // TODO: MODIFY PLACEHOLDER!
-                  {CBaseChainParams::Network::TESTNET,2000001}});       // TODO: MODIFY PLACEHOLDER!
+                  {CBaseChainParams::Network::TESTNET,1282000}});       // TODO: FOR PRIVATE TESTNET
 }
 }
