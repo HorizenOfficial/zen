dd9c192f507b126540ef9c1bff128ccc4fd661ee
Alessandro Petrini <apetrini@horizenlabs.io>
2023-08-10 17:50:53 +0200

    Refact: headers_07.py

--- qa/rpc-tests/headers_07.py
+++ qa/rpc-tests/headers_07.py
@@ -5,9 +5,12 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.authproxy import JSONRPCException
-from test_framework.util import initialize_chain_clean, start_nodes, \
-    sync_blocks, sync_mempools, connect_nodes_bi, disconnect_nodes
-import time
+from test_framework.util import initialize_chain_clean, start_nodes, mark_logs, \
+    sync_blocks, sync_mempools, connect_nodes_bi, disconnect_nodes, assert_equal, \
+    colorize as cc
+from headers_common import print_ordered_tips, get_block_finality
+
+DEBUG_MODE = 1
 
 class headers(BitcoinTestFramework):
 
@@ -15,12 +18,8 @@ class headers(BitcoinTestFramework):
         print("Initializing test directory "+self.options.tmpdir)
         initialize_chain_clean(self.options.tmpdir, 4)
 
-    def setup_nodes(self):
-        self.nodes = start_nodes(4, self.options.tmpdir)
-
     def setup_network(self, split=False):
         self.nodes = []
-
         self.nodes = start_nodes(4, self.options.tmpdir)
 
         if not split:
@@ -42,22 +41,6 @@ class headers(BitcoinTestFramework):
         connect_nodes_bi(self.nodes, 1, 3)
         self.is_network_split = False
 
-    def mark_logs(self, msg):
-        self.nodes[0].dbg_log(msg)
-        self.nodes[1].dbg_log(msg)
-        self.nodes[2].dbg_log(msg)
-        self.nodes[3].dbg_log(msg)
-
-    def dump_ordered_tips(self, tip_list):
-        sorted_x = sorted(tip_list, key=lambda k: k['status'])
-        c = 0
-        for y in sorted_x:
-            if (c == 0):
-                print(y)
-            else:
-                print(" ",y)
-            c = 1
-
     def run_test(self):
         '''
                (3)
@@ -70,18 +53,14 @@ class headers(BitcoinTestFramework):
         blocks = []
 
         blocks.append(self.nodes[0].getblockhash(0))
-        print("\n\nGenesis block is:\n" + blocks[0])
-
-        s = "Node 1 generates a block"
-        print("\n" + s)
-        self.mark_logs(s)
+        mark_logs(cc('e', "Genesis block is:         ") + blocks[0], self.nodes, DEBUG_MODE, color='n')
 
         blocks.extend(self.nodes[1].generate(1)) # block height 1
-        print(blocks[len(blocks)-1])
+        mark_logs(cc('c', "Node 1 generated a block: ") + blocks[len(blocks)-1], self.nodes, DEBUG_MODE, color='n')
         self.sync_all()
 
+        print_ordered_tips(self.nodes)
 #    Node(0): [0]->[1]
-#      |
 #      |
 #    Node(1): [0]->[1]
 #      /\
@@ -90,206 +69,179 @@ class headers(BitcoinTestFramework):
 #    |    
 # Node(3): [0]->[1]
 
-        print("\n\nSplit nodes (1)----x   x---(3)")
+        mark_logs("Split nodes (1)----x   x---(3)", self.nodes, DEBUG_MODE, color='b')
         self.split_network_2()
-
-        print("The network is split")
-        self.mark_logs("The network is split 2")
-
-        s = "Node 1 generates a block"
-        print("\n" + s)
-        self.mark_logs(s)
+        mark_logs("The network is split", self.nodes, DEBUG_MODE, color='e')
 
         blocks.extend(self.nodes[1].generate(1)) # block height 2
+        mark_logs(cc('c', "Node 1 generated a block: ") + blocks[2], self.nodes, DEBUG_MODE, color='n')
         bl2 = blocks[2]
-        print(bl2)
-        time.sleep(2)
+        sync_blocks(self.nodes[:3])
 
+        print_ordered_tips(self.nodes)
 #    Node(0): [0]->[1]->[2h]
-#      |
 #      |
 #    Node(1): [0]->[1]->[2h]
 #       \
 #        \
 #        Node(2): [0]->[1]->[2h]
-#      
+#
 # Node(3): [0]->[1]
 
-        print("\n\nSplit nodes (1)----x   x---(2)")
+        mark_logs("Split nodes (1)----x   x---(2)", self.nodes, DEBUG_MODE, color='b')
         self.split_network(1)
+        mark_logs("The network is split", self.nodes, DEBUG_MODE, color='e')
 
-        print("The network is split")
-        self.mark_logs("The network is split")
-
-        print("\nNode1 generating 7 honest block")
-        blocks.extend(self.nodes[1].generate(7)) # block height 3
+        blocks.extend(self.nodes[1].generate(7)) # block height 9
+        mark_logs("Node 1 generated 7 honest block", self.nodes, DEBUG_MODE, color='c')
         bl3 = blocks[3]
-        print(bl3)
-        time.sleep(2)
+        sync_blocks(self.nodes[:2])
 
-        print("\nNode3 generating 8 mal block")
-        blocks.extend(self.nodes[3].generate(8)) # block height 2M
-        for i in range(10, 18):
-            print(blocks[i])
-        time.sleep(2)
+        blocks.extend(self.nodes[3].generate(8)) # block height 10M
+        mark_logs("Node 3 generated 8 mal block", self.nodes, DEBUG_MODE, color='c')
+        sync_blocks([self.nodes[3]])
 
-#        raw_input("press enter to go on..")
+        blocks.extend(self.nodes[2].generate(8)) # block height 10m
+        mark_logs("Node 2 generated 8 mal block", self.nodes, DEBUG_MODE, color='c')
+        sync_blocks([self.nodes[2]])
 
-        print("\nNode2 generating 8 mal block")
-        blocks.extend(self.nodes[2].generate(8)) # block height 2m
-        for i in range(18, 26):
-            print(blocks[i])
-        time.sleep(2)
-
-#      Node(0): [0]->[1]->[2h]->[3h]
+        print_ordered_tips(self.nodes)
+#      Node(0): [0]->[1]->[2h]->[9h]
 #        |
+#      Node(1): [0]->[1]->[2h]->[9h]
+#
+#          Node(2): [0]->[1]->[2h]->[10m]
+#
+# Node(3): [0]->[1]->[9M]
+
+        mark_logs("Join network (1)--------(2)", self.nodes, DEBUG_MODE, color='b')
+        connect_nodes_bi(self.nodes, 1, 2)      # They are not going to sync their blocks, do not use join_network() here
+        sync_blocks(self.nodes, 2, False, 1)
+        mark_logs("The network has joined", self.nodes, DEBUG_MODE, color='e')
+
+        print_ordered_tips(self.nodes)
+#      Node(0): [0]->[1]->[2h]->[9h]      **Active**
+#        |                  \
+#        |                   +->[10m]
 #        |
-#      Node(1): [0]->[1]->[2h]->[3h]
-#          
-#           
-#          Node(2): [0]->[1]->[2h]->[3m]
-#        
-# Node(3): [0]->[1]->[2M]
-
-#        raw_input("press enter to go on..")
-
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
-
-        print("\n\nJoin nodes (1)--(2)")
-        # raw_input("press enter to join the netorks..")
-        self.mark_logs("Joining network")
-        connect_nodes_bi(self.nodes, 1, 2)
-#-------------------------------------------------
-
-        print("\nNetwork joined") 
-        self.mark_logs("Network joined")
-
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
-
-#      Node(0): [0]->[1]->[2h]->[3h]      **Active**
-#        |                  
-#        |                  
-#        |
-#      Node(1): [0]->[1]->[2h]->[3h]      **Active**
+#      Node(1): [0]->[1]->[2h]->[9h]      **Active**
 #         \                 \
-#          \                 +->[3m]
+#          \                 +->[10m]
 #           \
-#          Node(2): [0]->[1]->[2h]->[3m]  **Active**
+#          Node(2): [0]->[1]->[2h]->[10m]  **Active**
 #                               \ 
-#                                +->[3h] 
-#        
-# Node(3): [0]->[1]->[2M]
-
-#        raw_input("press enter to go on..")
+#                                +->[9h]
+#   Node(3): [0]->[1]->[9M]
 
         try:
-            print("\nChecking finality of block[", bl2, "]")
-            print("  Node0 has: %d" % self.nodes[0].getblockfinalityindex(bl2))
-            print("  Node1 has: %d" % self.nodes[1].getblockfinalityindex(bl2))
-            print("\nChecking finality of block[", bl3, "]")
-            print("  Node0 has: %d" % self.nodes[0].getblockfinalityindex(bl3))
-            print("  Node1 has: %d" % self.nodes[1].getblockfinalityindex(bl3))
+            mark_logs(f"Checking finality of block {bl2}", self.nodes, DEBUG_MODE, color='g')
+            get_block_finality(self.nodes, 0, bl2, 36)
+            get_block_finality(self.nodes, 1, bl2, 36)
+            mark_logs(f"Checking finality of block {bl3}", self.nodes, DEBUG_MODE, color='g')
+            get_block_finality(self.nodes, 0, bl3, 20)
+            get_block_finality(self.nodes, 1, bl3, 20)
         except JSONRPCException as e:
             errorString = e.error['message']
             print(errorString)
 
-#        raw_input("press enter to go on..")
-
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
-
-        print("\n\nJoin nodes (1)--(3)")
-        self.mark_logs("Joining network 2")
+        mark_logs("Join network (1)--------(3)", self.nodes, DEBUG_MODE, color='b')
         self.join_network_2()
+        mark_logs("The network has joined", self.nodes, DEBUG_MODE, color='e')
 
-        print("\nNetwork joined") 
-        self.mark_logs("Network joined 2")
-        time.sleep(2)
-
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
-
-#     Node(0): [0]->[1]->[2h]->[3h]      **Active**
+        print_ordered_tips(self.nodes)
+#     Node(0): [0]->[1]->[2h]->[9h]      **Active**
 #       |                  \
-#       |                   +->[3m]
+#       |                   +->[10m]
 #       |
-#       |              +->[2M]
+#       |              +->[9M]
 #       |             /
-#     Node(1): [0]->[1]->[2h]->[3h]      **Active**
+#     Node(1): [0]->[1]->[2h]->[9h]      **Active**
 #       /\                  \
-#      /  \                  +->[3m]
+#      /  \                  +->[10m]
 #     /    \
-#    +    Node(2): [0]->[1]->[2h]->[3m]  **Active**
+#    +    Node(2): [0]->[1]->[2h]->[10m]  **Active**
 #    |                         \ 
-#    |                          +->[3h] 
+#    |                          +->[9h] 
 #    |
-# Node(3): [0]->[1]->[2M]
+# Node(3): [0]->[1]->[9M]
 #                 \
-#                  +->[2h]->[3h]     **Active**
+#                  +->[2h]->[9h]     **Active**
 
-#        raw_input("press enter to go on..")
+        blocks.extend(self.nodes[3].generate(40)) # block height 49M
+        mark_logs("Node 3 generated 40 mal blocks: ", self.nodes, DEBUG_MODE, color='c')
+        sync_blocks(self.nodes)
 
-        print("\nNode3 generating 40 mal block")
-        blocks.extend(self.nodes[3].generate(40)) # block height 4m
-        n=len(blocks)-1
-        print(blocks[n])
-        time.sleep(3)
-
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
+        print_ordered_tips(self.nodes)
+#                      +-------------->[49M]  **Active**
+#                     /
+#     Node(0): [0]->[1]->[2h]->[9h]
+#       |                  \
+#       |                   +->[10m]
+#       |
+##      |              +------------->[49M]  **Active**
+#       |             /
+#     Node(1): [0]->[1]->[2h]->[9h]
+#       /\                  \
+#      /  \                  +->[10m]
+#     /    \
+#    |      |              +---------->[49M]  **Active**
+#    |      |             /
+#    +    Node(2): [0]->[1]->[2h]->[10m]
+#    |                         \ 
+#    |                          +->[9h] 
+#    |
+# Node(3): [0]->[1]------------------->[49M]  **Active**
+#                 \
+#                  +->[2h]->[9h]
 
         try:
-            print("\nChecking finality of block[", bl2, "]")
-            print("  Node0 has: %d" % self.nodes[0].getblockfinalityindex(bl2))
-            print("  Node1 has: %d" % self.nodes[1].getblockfinalityindex(bl2))
-            print("\nChecking finality of block[", bl3, "]")
-            print("  Node0 has: %d" % self.nodes[0].getblockfinalityindex(bl3))
-            print("  Node1 has: %d" % self.nodes[1].getblockfinalityindex(bl3))
+            mark_logs(f"Checking finality of block {bl2}", self.nodes, DEBUG_MODE, color='g')
+            get_block_finality(self.nodes, 0, bl2, -1)  # Actual value does not matter, this should fail here
         except JSONRPCException as e:
             errorString = e.error['message']
-            print(errorString, "\n")
+            mark_logs(errorString, self.nodes, DEBUG_MODE, color='y')
+            mark_logs(f"===> Malicious attach succeeded!", self.nodes, DEBUG_MODE, color='y')
 
-        print("\nNode2 generating 40 mal block")
-        blocks.extend(self.nodes[2].generate(40)) # block height 4m
-        n=len(blocks)-1
-        print(blocks[n])
-        time.sleep(3)
 
-        self.mark_logs("\nSyncing network after malicious attack")
+        blocks.extend(self.nodes[2].generate(40)) # block height 89m
+        mark_logs("Node 2 generated 40 mal blocks: ", self.nodes, DEBUG_MODE, color='c')
+
+        mark_logs("Syncing network after malicious attack", self.nodes, DEBUG_MODE, color='e')
         sync_blocks(self.nodes, 1, True, 10)
 
-#     Node(0): [0]->[1]->[2h]->[3h]     
+        print_ordered_tips(self.nodes)
+#                      +-------------->[49M]->[89m]  **Active**
+#                     /
+#     Node(0): [0]->[1]->[2h]->[9h]
 #       |                  \
-#       |                   +->[3m]->[4m]       **Active**
+#       |                   +->[10m]
 #       |
-#       |              +->[2M]
+##      |              +------------->[49M]->[89m]  **Active**
 #       |             /
-#     Node(1): [0]->[1]->[2h]->[3h]  
+#     Node(1): [0]->[1]->[2h]->[9h]
 #       /\                  \
-#      /  \                  +->[3m]->[4m]      **Active**
-#     /    \
-#    +    Node(2): [0]->[1]->[2h]->[3m]->[4m]   **Active**
+#      /  \                  +->[10m]
+#     |    |
+#     |    |               +---------->[49M]->[89m]  **Active**
+#     /    \              /
+#    +    Node(2): [0]->[1]->[2h]->[10m]
 #    |                         \ 
-#    |                          +->[3h] 
+#    |                          +->[9h] 
 #    |
-# Node(3): [0]->[1]->[2M]
+# Node(3): [0]->[1]------------------->[49M]->[89m]  **Active**
 #                 \
-#                  +->[2h]->[3h] 
-#                       \
-#                        +->[3m]->[4m]          **Active**
+#                  +->[2h]->[9h]
 
-        for i in range(0, 4):
-            self.dump_ordered_tips(self.nodes[i].getchaintips())
-            print("---")
+        mark_logs("Check that all the nodes have the same active tip", self.nodes, DEBUG_MODE, color='g')
+        node0ActiveTip = [tip for tip in self.nodes[0].getchaintips() if tip['status'] == 'active'][0]
+        node1ActiveTip = [tip for tip in self.nodes[1].getchaintips() if tip['status'] == 'active'][0]
+        node2ActiveTip = [tip for tip in self.nodes[2].getchaintips() if tip['status'] == 'active'][0]
+        node3ActiveTip = [tip for tip in self.nodes[3].getchaintips() if tip['status'] == 'active'][0]
+        assert_equal(node0ActiveTip['hash'], blocks[len(blocks)-1])
+        assert_equal(node1ActiveTip['hash'], blocks[len(blocks)-1])
+        assert_equal(node2ActiveTip['hash'], blocks[len(blocks)-1])
+        assert_equal(node3ActiveTip['hash'], blocks[len(blocks)-1])
 
-#        raw_input("press enter to go on..")
 
 if __name__ == '__main__':
     headers().main()
