778df49ee39be20fd91eb665465afbd1af1bf94b
GiacomoPirinoli <gpirinoli@horizenlabs.io>
2023-11-27 16:38:51 +0100

    Extended test for mempool cleaning after hard fork

--- qa/rpc-tests/mempool_hard_fork_cleaning.py
+++ qa/rpc-tests/mempool_hard_fork_cleaning.py
@@ -36,6 +36,7 @@ class Test (BitcoinTestFramework):
         # ####################################################################################################
         # Fork 8 - Sidechain hard fork
         # ####################################################################################################
+        mark_logs("********** Fork 8 - Sidechain hard fork **********", self.nodes, DEBUG_MODE)
 
         mark_logs("Node 0 starts mining to reach the block immediately before the sidechain hard fork activation", self.nodes, DEBUG_MODE)
         sc_fork_height = ForkHeights['MINIMAL_SC']
@@ -60,65 +61,100 @@ class Test (BitcoinTestFramework):
 
 
 
+        # Preparation for fork12: some shielded notes are created (needed later, but must be prepared before fork11 occurs)
+        notes_to_create = 3
+        for i in range(notes_to_create):
+            opid = self.nodes[0].z_mergetoaddress(["ANY_TADDR"], self.nodes[0].z_getnewaddress(), Z_FEE, 2, 0)["opid"]
+            wait_and_assert_operationid_status(self.nodes[0], opid)
+            self.sync_all()
+        self.nodes[1].generate(1)
+        self.sync_all()
+
         # ####################################################################################################
         # Fork 11 - Shielded pool deprecation hard fork
         # ####################################################################################################
+        # ####################################################################################################
+        # Fork 12 - Unshielding to script only hard fork
+        # ####################################################################################################
+        for fork in range(11, 12 + 1):
+            if (fork == 11):
+                fork_descr = "Shielded Pool Deprecation"
+                fork_height = ForkHeights['SHIELDED_POOL_DEPRECATION']
+                tx_type_descr = "shielding transaction"
+            elif (fork == 12):
+                fork_descr = "Unshielding To Script Only"
+                fork_height = ForkHeights['UNSHIELDING_TO_SCRIPT_ONLY']
+                tx_type_descr = "unshielding transaction to publick-key-address"
 
-        mark_logs("Node 0 starts mining to get close to the Shieleded Pool Deprecation hard fork [Fork - 3]", self.nodes, DEBUG_MODE)
-        shielded_pool_deprecation_fork_height = ForkHeights['SHIELDED_POOL_DEPRECATION']
-        block_count = self.nodes[0].getblockcount()
-        self.nodes[1].generate(shielded_pool_deprecation_fork_height - block_count - 3)
-        self.sync_all()
+            mark_logs(f"********** Fork {fork} - {fork_descr} hard fork **********", self.nodes, DEBUG_MODE)
 
-        mark_logs("Split the network", self.nodes, DEBUG_MODE)
-        self.split_network(0)
+            mark_logs(f"Node 0 starts mining to get close to the {fork_descr} hard fork [Fork - 3]", self.nodes, DEBUG_MODE)
+            block_count = self.nodes[0].getblockcount()
+            self.nodes[1].generate(fork_height - block_count - 3)
+            self.sync_all()
 
-        mark_logs("Node 0 creates a shielding transaction", self.nodes, DEBUG_MODE)
-        total_funds_before_shielding = sum(x['amount'] for x in self.nodes[0].listunspent(0))
-        opid = self.nodes[0].z_mergetoaddress(["ANY_TADDR"], self.nodes[0].z_getnewaddress(), Z_FEE, 2, 2)["opid"]
-        wait_and_assert_operationid_status(self.nodes[0], opid)
+            mark_logs("Split the network", self.nodes, DEBUG_MODE)
+            self.split_network(0)
 
-        # Give time to the wallet to be notified and updated the balance (listunspent would otherwise return an outdated value)
-        wait_until(lambda: self.nodes[0].getmempoolinfo()['fullyNotified'] == True, 20)
-        assert_greater_than(total_funds_before_shielding, sum(x['amount'] for x in self.nodes[0].listunspent(0))) # Some less funds available
+            mark_logs(f"Node 0 creates {tx_type_descr}", self.nodes, DEBUG_MODE)
+            if (fork == 11):
+                total_funds_before = sum(x['amount'] for x in self.nodes[0].listunspent(0))
+                opid = self.nodes[0].z_mergetoaddress(["ANY_TADDR"], self.nodes[0].z_getnewaddress(), Z_FEE, 2, 0)["opid"]
+            elif (fork == 12):
+                total_funds_before = sum(x['amount'] for x in self.nodes[0].z_listunspent(0))
+                opid = self.nodes[0].z_mergetoaddress(["ANY_ZADDR"], self.nodes[0].getnewaddress(), Z_FEE, 0, 2)["opid"]
+            wait_and_assert_operationid_status(self.nodes[0], opid)
 
-        mark_logs("Join the network", self.nodes, DEBUG_MODE)
-        self.join_network(0)
+            # Give time to the wallet to be notified and updated the balance (listunspent would otherwise return an outdated value)
+            wait_until(lambda: self.nodes[0].getmempoolinfo()['fullyNotified'] == True, 20)
+            if (fork == 11):
+                assert_greater_than(total_funds_before, sum(x['amount'] for x in self.nodes[0].listunspent(0))) # Some less funds available
+            elif (fork == 12):
+                assert_greater_than(total_funds_before, sum(x['amount'] for x in self.nodes[0].z_listunspent(0))) # Some less funds available
 
-        mark_logs("Check that the shielding transaction is only available on node 0 (no rebroadcasting happens)", self.nodes, DEBUG_MODE)
-        assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
-        assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
+            mark_logs("Join the network", self.nodes, DEBUG_MODE)
+            self.join_network(0)
 
-        mark_logs("Node 1 mines one block, new height is [Fork - 2]", self.nodes, DEBUG_MODE)
-        self.nodes[1].generate(1)
-        sync_blocks(self.nodes)
+            mark_logs(f"Check that the {tx_type_descr} is only available on node 0 (no rebroadcasting happens)", self.nodes, DEBUG_MODE)
+            assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
+            assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
 
-        mark_logs("Check that the mempool situation is unchanged", self.nodes, DEBUG_MODE)
-        assert_equal(self.nodes[0].getblockcount(), shielded_pool_deprecation_fork_height - 2)
-        assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
-        assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
+            mark_logs("Node 1 mines one block, new height is [Fork - 2]", self.nodes, DEBUG_MODE)
+            self.nodes[1].generate(1)
+            sync_blocks(self.nodes)
 
-        mark_logs("Node 1 mines another block, new height is [Fork - 1] (shielding transaction can't be mined in the next block)", self.nodes, DEBUG_MODE)
-        self.nodes[1].generate(1)
-        sync_blocks(self.nodes)
+            mark_logs("Check that the mempool situation is unchanged", self.nodes, DEBUG_MODE)
+            assert_equal(self.nodes[0].getblockcount(), fork_height - 2)
+            assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
+            assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
 
-        mark_logs("Check that the shielding transaction has been evicted", self.nodes, DEBUG_MODE)
-        assert_equal(self.nodes[0].getblockcount(), shielded_pool_deprecation_fork_height - 1)
-        assert_equal(self.nodes[0].getmempoolinfo()["size"], 0)
-        assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
+            mark_logs(f"Node 1 mines another block, new height is [Fork - 1] ({tx_type_descr} can't be mined in the next block)", self.nodes, DEBUG_MODE)
+            self.nodes[1].generate(1)
+            sync_blocks(self.nodes)
 
-        mark_logs("Check that the shielding transaction is not rebroadcasted even in case of explicit request", self.nodes, DEBUG_MODE)
-        resenttxs = self.nodes[0].resendwallettransactions()
-        self.sync_all()
-        assert_equal(len(resenttxs), 0)
+            mark_logs(f"Check that the {tx_type_descr} has been evicted", self.nodes, DEBUG_MODE)
+            assert_equal(self.nodes[0].getblockcount(), fork_height - 1)
+            assert_equal(self.nodes[0].getmempoolinfo()["size"], 0)
+            assert_equal(self.nodes[1].getmempoolinfo()["size"], 0)
 
-        mark_logs("Check that the funds used for the shielding transaction are available again", self.nodes, DEBUG_MODE)
-        assert_equal(sum(x['amount'] for x in self.nodes[0].listunspent(0)), total_funds_before_shielding)
-        self.nodes[0].sendtoaddress(self.nodes[0].getnewaddress(), total_funds_before_shielding, "", "", True)
-        assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
-        self.sync_all()
+            mark_logs(f"Check that the {tx_type_descr} is not rebroadcasted even in case of explicit request", self.nodes, DEBUG_MODE)
+            resenttxs = self.nodes[0].resendwallettransactions()
+            self.sync_all()
+            assert_equal(len(resenttxs), 0)
 
-        mark_logs("RemoveStaleTransaction() function worked properly for block connection on a hard fork", self.nodes, DEBUG_MODE)
+            mark_logs(f"Check that the funds used for the {tx_type_descr} are available again", self.nodes, DEBUG_MODE)
+            if (fork == 11):
+                assert_equal(sum(x['amount'] for x in self.nodes[0].listunspent(0)), total_funds_before)
+                self.nodes[0].sendtoaddress(self.nodes[0].getnewaddress(), total_funds_before, "", "", True)
+            elif (fork == 12):
+                assert_equal(sum(x['amount'] for x in self.nodes[0].z_listunspent(0)), total_funds_before)
+                notes_quantity = len(self.nodes[0].z_listunspent(0))
+                opid = self.nodes[0].z_mergetoaddress(["ANY_ZADDR"], self.nodes[0].z_getnewaddress(), Z_FEE, 0, notes_quantity)["opid"]
+                wait_and_assert_operationid_status(self.nodes[0], opid)
+            assert_equal(self.nodes[0].getmempoolinfo()["size"], 1)
+            self.sync_all()
+
+            mark_logs("RemoveStaleTransaction() function worked properly for block connection on a hard fork", self.nodes, DEBUG_MODE)
 
 
 if __name__ == '__main__':
