24b958ff9f6d5702f6163951ef31f5cd9c4177c2
Daniele Rogora <danielerogora@horizenlabs.io>
2024-01-12 16:45:36 +0100

    Enable garbage collection and O3 builds with clang

--- depends/hosts/linux_clang.mk
+++ depends/hosts/linux_clang.mk
@@ -1,7 +1,8 @@
 linux_CFLAGS=-pipe
 linux_CXXFLAGS=$(linux_CFLAGS)
+linux_LDFLAGS=-Wl,--gc-sections
 
-linux_release_CFLAGS=-O1
+linux_release_CFLAGS=-O3 -fvisibility=hidden
 linux_release_CXXFLAGS=$(linux_release_CFLAGS)
 
 linux_debug_CFLAGS=-O1
